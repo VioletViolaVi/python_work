06bba62c48e1e5f9d3236ddfc2854be5396819af
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-13 23:16:01 +0000

    add practice

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# input()
+
+user_name = input("Enter username: ")
+user_height = input("Enter your height in cm: ")
+
+print(f"user_height is type: {type(user_height)}")
+increased_user_height = int(user_height) + 5
+
+print(f"Welcome back: {user_name}!")
+print(f"You are {user_height}cm tall!")
+print(f"5cm more and you will be {increased_user_height}cm tall!")
