2a3363e19356d9b19805492e764b46fe204d8279
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-13 23:29:03 +0000

    create rectangle area calculator

--- user_inputs/rectangle_area.py
+++ user_inputs/rectangle_area.py
@@ -1,2 +1,16 @@
-# Calculate area of a rectangle (length * breath)
+# Calculate area of a rectangle (length * breadth)
 
+user_length = int(input("please enter the length: "))
+user_breadth = int(input("please enter the breadth: "))
+
+rect_area = user_length * user_breadth
+
+print(f"The area of your rectangle is: {rect_area}cm2")
+
+
+print("----- checks below ----- ")
+print(f"user_length is: {user_length}")
+print(f"user_length TYPE is: {type(user_length)}")
+
+print(f"user_breadth is: {user_breadth}")
+print(f"user_breadth TYPE is: {type(user_breadth)}")
