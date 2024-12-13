67996e9cc6d62bc6fa936199a131185ad99b90d6
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-13 22:15:51 +0000

    add practice examples to file

--- variables/booleans.py
+++ variables/booleans.py
@@ -1,3 +1,23 @@
 # Booleans
+is_human = False
+is_raining = True
+is_password = False
+
+print(f"Are you human?: {is_human}")
 
 
+if is_human:
+    print("Yes, I am human!")
+else:
+    print("No, I am an alien!")
+
+
+if is_raining:
+    print("It is raining outside.")
+else:
+    print("It is NOT raining outside.")
+
+if is_password:
+    print("Password Accepted!")
+else:
+    print("Password Failed!")
