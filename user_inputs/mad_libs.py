70f899e2f2c69ab800dddb292465c8bb9816c472
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-14 22:29:36 +0000

    complete madlib example

--- user_inputs/mad_libs.py
+++ user_inputs/mad_libs.py
@@ -1,7 +1,11 @@
 # Mad Libs
 
-print(f"In school, I met two really {adj1} kids. All of us 
-      became {noun1} very fast. That day we had a {adj2} 
-      lesson, and luckily my friends and I were at the same 
-      {noun2}. My friend's names are {noun3} and {noun3}.")
+adj1 = input("Enter your 1st adjective: ")
+adj2 = input("Enter your 2nd adjective: ")
+adj3 = input("Enter your 3rd adjective: ")
+noun2 = input("Enter your 1st noun: ")
+noun3 = input("Enter your 2nd noun: ")
+noun4 = input("Enter your 3rd noun: ")
+
+print(f"In school, I met two really {adj1} kids. All of us became {adj2} very fast. That day we had a {adj3} lesson, and luckily my friends and I were at the same {noun2}. My friend's names are {noun3} and {noun4}.")
 
