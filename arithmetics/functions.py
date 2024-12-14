d8b49c7ba24f121b7cf5e400b5e9a3613025d7f3
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-14 23:11:30 +0000

    add math function practice

---
+++ arithmetics/functions.py
@@ -0,0 +1,25 @@
+#  Math Functions
+
+total = 12.84
+round_total = round(total)
+print(round_total)
+
+total_2 = 16
+round_total_2 = pow(total_2, 2)
+print(round_total_2)
+
+total_3 = 5021
+round_total_3 = abs(total_3)
+print(round_total_3)
+
+player_1 = 8
+player_2 = 4
+player_3 = 7
+high_score = max(player_1, player_2, player_3)
+print(high_score)
+
+player_1 = 8
+player_2 = 4
+player_3 = 7
+lowest_score = min(player_1, player_2, player_3)
+print(lowest_score)
