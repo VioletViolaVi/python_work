386a551eeb642733a7a6961ac16c0b2681820498
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-14 22:53:27 +0000

    add practice

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -1,7 +1,23 @@
 #  Arithmetic Operators
 
-laptops = 0
+laptops = 10
 
 laptops = laptops + 1
+laptops += 1
+
+laptops = laptops - 1
+laptops -= 1
+
+laptops = laptops * 5
+laptops *= 5
+
+laptops = laptops / 2
+laptops /= 2
+
+laptops = laptops **3
+laptops **= 3
+
+laptops = laptops % 6
+laptops %= 6
 
 print(laptops)
