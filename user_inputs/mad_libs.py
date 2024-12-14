b6f6fbc8317d30a795f5b3fd39c13f93758c0d67
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-14 20:37:41 +0000

    create file & add madlib

---
+++ user_inputs/mad_libs.py
@@ -0,0 +1,7 @@
+# Mad Libs
+
+print(f"In school, I met two really {adj1} kids. All of us 
+      became {noun1} very fast. That day we had a {adj2} 
+      lesson, and luckily my friends and I were at the same 
+      {noun2}. My friend's names are {noun3} and {noun3}.")
+
