cad37b3f6bbf1605b0b8cb7bd968f868acb59b44
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-15 18:58:08 +0000

    add practice using user input to calculate circle area

--- arithmetics/circle_area.py
+++ arithmetics/circle_area.py
@@ -2,3 +2,24 @@
 
 import math
 
+radius = 3
+
+squared_radius = radius ** 2
+
+circle_area = math.pi * squared_radius
+
+print(circle_area)
+
+
+
+print("--- user input version ---")
+
+
+
+user_radius = float(input("Enter numerical value for radius: "))
+
+user_squared_radius = user_radius ** 2
+
+user_circle_area = math.pi * user_squared_radius
+
+print(round(user_circle_area, 2))
