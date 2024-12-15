a1a023bb7b43ca2f6968f92472d2580b5628150d
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-15 18:48:14 +0000

    create user input version of circle circumference practice

--- arithmetics/circle_circumference.py
+++ arithmetics/circle_circumference.py
@@ -8,3 +8,15 @@ circumference = 2 * math.pi * radius
 
 print(circumference)
 print(round(circumference, 2))
+
+
+
+print("--- user input version ---")
+
+
+
+user_radius = float(input("Enter a numerical radius value: "))
+print(user_radius)
+
+user_circumference = round(2 * math.pi * user_radius, 2)
+print(f"The circumference of your circle, with the radius of '{user_radius}cm' is '{user_circumference}cm2'! :D")
