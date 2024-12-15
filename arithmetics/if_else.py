f65d39d922fe8e21cddeecd5f4a05d92ecc6cb6b
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-15 20:03:02 +0000

    practice new example with more 'elif' statements

--- arithmetics/if_else.py
+++ arithmetics/if_else.py
@@ -12,11 +12,30 @@ else:
 print(" --- new example --- ")
 
 
-user_topping = input("Do you prefer pizza or spaghetti?: ")
+user_meal = input("Do you prefer pizza or spaghetti?: ")
 
-if user_topping == "pizza":
+if user_meal == "pizza":
     print("You selected PIZZA")
-elif user_topping == "spaghetti":
+elif user_meal == "spaghetti":
     print("You selected SPAGHETTI")
 else:
     print("You DID NOT choose between pizza or spaghetti!")
+
+
+
+user_topping = input("Pick a pizza topping (pepperoni, ham, spinach, bacon, onions, extra cheese): ")
+
+if user_topping == "pepperoni":
+    print("You added pepperoni to your pizza")
+elif user_topping == "ham":
+    print("You added ham to your pizza")
+elif user_topping == "spinach":
+    print("You added spinach to your pizza")
+elif user_topping == "bacon":
+    print("You added bacon to your pizza")
+elif user_topping == "onions":
+    print("You added onions to your pizza")
+elif user_topping == "extra cheese":
+    print("You added extra cheese to your pizza")
+else:
+    print("You must pick from the provided options!")
