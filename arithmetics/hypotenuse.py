ae1180e12f6b23ecb569574f96daffb1b82fa039
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-15 19:20:11 +0000

    calculate hypotenuse using math module

--- arithmetics/hypotenuse.py
+++ arithmetics/hypotenuse.py
@@ -1,3 +1,11 @@
 # Right Angled Triangle Hypotenuse
 
 import math
+
+opposite = float(input("Enter numerical opposite value: "))
+
+adjacent = float(input("Enter numerical adjacent value: "))
+
+hypotenuse = math.sqrt( pow(opposite, 2) + pow(adjacent, 2) )
+
+print(hypotenuse)
