980a006167a85d8c1a6685826de71fc4acc995fd
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-15 21:10:07 +0000

    create calculator using if/elif/else statements

--- conditionals/calculator.py
+++ conditionals/calculator.py
@@ -1,4 +1,21 @@
 # Calculator via Conditional Statements
 
+operator = input("Select an operator ( + | - | / | * ): ")
+num_1 = float(input("Enter 1st value: "))
+num_2 = float(input("Enter 2nd value: "))
+answer = 0
 
-
+if operator == "+":
+    answer = num_1 + num_2
+    print(f"{num_1} + {num_2} = {answer}")
+elif operator == "-":
+    answer = num_1 - num_2
+    print(f"{num_1} - {num_2} = {answer}")
+elif operator == "/":
+    answer = num_1 / num_2
+    print(f"{num_1} / {num_2} = {answer}")
+elif operator == "*":
+    answer = num_1 * num_2
+    print(f"{num_1} * {num_2} = {answer}")
+else:
+    print("Error! Error! Error!")
