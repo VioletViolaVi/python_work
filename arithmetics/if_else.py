f3c2297c73ad9d0c0e3ee1b6dba25ef77fa0bcdd
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-15 19:42:24 +0000

    create conditional statements based on user input

--- arithmetics/if_else.py
+++ arithmetics/if_else.py
@@ -1,3 +1,9 @@
 # If Else Statements
 
+user_age = int(input("How old are you?: "))
 
+if user_age >= 18:
+    print("You are old enough to drink beer!")
+else:
+    print("You are NOT old enough to drink beer!")
+    
