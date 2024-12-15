61f0e42fac8f51d19b8bbf12981506b84e82d72e
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-15 19:53:33 +0000

    add new statement containing 'elif'

--- arithmetics/if_else.py
+++ arithmetics/if_else.py
@@ -6,4 +6,17 @@ if user_age >= 18:
     print("You are old enough to drink beer!")
 else:
     print("You are NOT old enough to drink beer!")
-    
+
+
+
+print(" --- new example --- ")
+
+
+user_topping = input("Do you prefer pizza or spaghetti?: ")
+
+if user_topping == "pizza":
+    print("You selected PIZZA")
+elif user_topping == "spaghetti":
+    print("You selected SPAGHETTI")
+else:
+    print("You DID NOT choose between pizza or spaghetti!")
