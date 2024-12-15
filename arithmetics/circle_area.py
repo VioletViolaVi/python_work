0a042dc942154f8ee51f44b397b618a02d25129b
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-15 19:08:11 +0000

    use math.pow() method to calculate circle area

--- arithmetics/circle_area.py
+++ arithmetics/circle_area.py
@@ -29,3 +29,19 @@ user_circle_area = math.pi * user_squared_radius
 print(f"user_circle_area: {user_circle_area}")
 
 print(round(user_circle_area, 2))
+
+
+
+print("--- (shorter) user input version ---")
+
+
+
+entered_radius = float(input("Enter numerical value for radius: "))
+
+entered_circle_area = math.pi * pow(entered_radius, 2)
+
+print(f"entered_radius: {entered_radius}")
+
+print(f"entered_circle_area: {entered_circle_area}")
+
+print(round(entered_circle_area, 2))
