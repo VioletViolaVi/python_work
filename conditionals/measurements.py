a7adce5ace7ec2d333f3b45c3fee666f803f40ef
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-16 15:07:20 +0000

    complete conditional statements to convert cm to m

--- conditionals/measurements.py
+++ conditionals/measurements.py
@@ -9,6 +9,8 @@ user_unit_choice = input("Select unit measurement ('mm', 'cm', 'm'): ").lower()
 # converting from mm
 if user_unit_choice == "mm":
     user_conversion_choice = input("Select unit to convert to ('cm' or 'm'): ").lower()
+
+    # mm --> cm / m / error
     if user_conversion_choice == "cm":
         result = user_height / 10
         print(f"Your {user_height}{user_unit_choice} height is {result}{user_conversion_choice}!")
@@ -16,14 +18,20 @@ if user_unit_choice == "mm":
         result = user_height / 1000
         print(f"Your {user_height}{user_unit_choice} height is {result}{user_conversion_choice}!")
     else:
-        print("You did not pick between 'cm' or 'm'!!!")
+        print("You did not pick between 'cm' or 'm'!")
 
 # converting from cm
 elif user_unit_choice == "cm":
     user_conversion_choice = input("Select unit to convert to ('mm' or 'm'): ").lower()
+
+    # cm --> mm / m / error
     if user_conversion_choice == "mm":
         result = user_height * 10
         print(f"Your {user_height}{user_unit_choice} height is {result}{user_conversion_choice}!")
+    elif user_conversion_choice == "m":
+        result = user_height / 100
+        print(f"Your {user_height}{user_unit_choice} height is {result}{user_conversion_choice}!")
+
 
 
 
