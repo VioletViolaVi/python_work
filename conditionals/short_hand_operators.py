744720a40f48a55d07f1efb6fb06a52096895b2e
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-16 21:04:16 +0000

    add more short hand practice

--- conditionals/short_hand_operators.py
+++ conditionals/short_hand_operators.py
@@ -2,6 +2,30 @@
 
 # mine
 if True: print("This is truthy!")
+
+
+
+print(" --- next --- ")
+
+
+
 print("5 is bigger than 10") if 5 > 10 else print("5 is NOT bigger than 10")
 
-# From vid
+# following vid structure
+print("15 is bigger than 10" if 15 > 10 else "15 is NOT bigger than 10")
+
+num = 5
+print("+ve" if num > 0 else "-ve")
+
+
+
+print(" --- next --- ")
+
+
+
+# Odd or Even?
+num = 141
+
+print("Odd number" if not num % 2 == 0 else "Even number")
+
+print("Odd number") if not num % 2 == 0 else print("Even number")
