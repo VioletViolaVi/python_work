3a4550d9dcee22655dc10ffc73ac5547445c890e
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-16 19:07:39 +0000

    create code to check legal age

--- logical_operators.py
+++ logical_operators.py
@@ -1,1 +1,16 @@
-# Logical Operators
+# Logical Operators: and | or | not
+
+# Going Clubbing
+age = -26
+has_id = False
+
+# Getting inside club
+if age >= 18 and has_id:
+    print("You can go into the club!🍻🍸")
+elif age >= 18 and not has_id:
+    print("You need id to prove you're of age! 🪪")
+elif age < 18 and has_id:
+    print("You are not old enough to enter!")
+else:
+    print("Something has gone wrong! 🤯🫨 😱😲")
+
