d893c34a5ebae77f026279bd8c7e3cd81e688678
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-16 15:13:04 +0000

    complete conditional statements to convert height from m to error

--- conditionals/measurements.py
+++ conditionals/measurements.py
@@ -45,6 +45,8 @@ elif user_unit_choice == "m":
     elif user_conversion_choice == "cm":
         result = user_height * 100
         print(f"Your {user_height}{user_unit_choice} height is {result}{user_conversion_choice}!")
+    else:
+        print("You did not pick between 'mm' or 'cm'!")
 
         
 
