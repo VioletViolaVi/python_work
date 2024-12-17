d28ae6c5ba3e4d791ab756832ea917ef430c8986
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-17 23:55:36 +0000

    practice formatting spaces with & without 0's

--- format_specifiers.py
+++ format_specifiers.py
@@ -2,16 +2,31 @@
 
 # Notes:
     # format specifiers -> {values:flags}
-    # {value:.2f} / {value:.0f} / {value:.5f} etc. for decimal places (adds '0's if not enough nums @ end)
-    # 
+    # {value:.2f} / {value:.0f} / {value:.5f} etc. -> for decimal places (adds 0's if not enough nums @ end)
+    # {value:7} / {value:6} / {value:2} etc. -> adds spaces in front to total 7, 6 and 2 spaces in output
+    # {value:07} / {value:06} / {value:02} etc. -> adds 0's instead of spaces in output
     # 
     # #
 
-# prices
 chocolate_price = 5632.89534
 ice_cream_price = 2156.253626
 juice_price = -6532.0763
 
-print(f"chocolate price: £{chocolate_price:.9f}")
-print(f"ice cream price: £{ice_cream_price:.2f}")
-print(f"juice price: £{juice_price:.1f}")
+# decimal places
+print(f"9 decimal places: £{chocolate_price:.9f}")
+print(f"2 decimal places: £{ice_cream_price:.2f}")
+print(f"1 decimal place: £{juice_price:.1f}")
+
+print(" --- next ---")
+
+# adding spaces in front
+print(f"20 spaces total: £{chocolate_price:20}")
+print(f"21 spaces total: £{ice_cream_price:21}")
+print(f"15 spaces total: £{juice_price:15}")
+
+print(" --- next ---")
+
+# adding 0's in front
+print(f"20 spaces total: £{chocolate_price:020}")
+print(f"21 spaces total: £{ice_cream_price:021}")
+print(f"15 spaces total: £{juice_price:015}")
