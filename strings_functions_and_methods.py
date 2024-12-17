43a08616be5b2b9bb92621b62a484ec7293e2ec1
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-17 00:04:05 +0000

    practice using .find() string method

---
+++ strings_functions_and_methods.py
@@ -0,0 +1,30 @@
+# String Functions & Methods
+
+# len()
+username = input("Enter your name: ") # Jane Doe used
+print(len(username))
+
+
+
+print(" --- next --- ")
+
+
+
+# .find()
+j_index = username.find('J')
+a_index = username.find('a')
+n_index = username.find('n')
+e_index = username.find('e')
+space_index = username.find(' ')
+d_index = username.find('D')
+o_index = username.find('o')
+e_index = username.find('e')
+
+print(f"J => {j_index}")
+print(f"a => {a_index}")
+print(f"n => {n_index}")
+print(f"e => {e_index}")
+print(f"' ' => {space_index}")
+print(f"D => {d_index}")
+print(f"o => {o_index}")
+print(f"e => {e_index}")
