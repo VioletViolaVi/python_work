f7ca12d6ef18ee420615810b8ab41942b6932530
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-17 23:42:05 +0000

    practice formatting decimal places

--- format_specifiers.py
+++ format_specifiers.py
@@ -1,2 +1,10 @@
-# Format Specifiers
+# Format Specifiers - vid followed
 
+# prices
+chocolate_price = 5632.89534
+ice_cream_price = 2156.253626
+juice_price = -6532.0763
+
+print(f"chocolate price: £{chocolate_price:.9f}")
+print(f"ice cream price: £{ice_cream_price:.2f}")
+print(f"juice price: £{juice_price:.1f}")
