f145722ee4020a6f3febafa23e41b9381f652bd5
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-17 13:06:45 +0000

    swap .isdigit() for .isalpha() and make progress with exercise

--- strings/username_validation.py
+++ strings/username_validation.py
@@ -10,13 +10,19 @@ username = input("Please enter a valid username: ")
 
 check_char_len = len(username) # needs to be 12 or less
 check_spaces = username.find(" ") # needs to be -1
-check_digits = username.isdigit() # needs to be False
+check_only_letters = username.isalpha() # needs to be True
 
-print(f"check_char_len: {check_char_len}")
-print(f"check_spaces: {check_spaces}")
-print(f"check_digits: {check_digits}")
+print(f"check_char_len: {check_char_len} (needs to be 12 or less)")
+print(f"check_spaces: {check_spaces} (needs to be -1)")
+print(f"check_only_letters: {check_only_letters} (needs to be True)")
 
 if check_char_len <= 12:
     if check_spaces == -1:
-        if not check_digits:
+        if check_only_letters:
             print("Yeah! You have a valid username! 🎊 🥳 🙌")
+        else:
+            print("Username must have no digits!")
+    else:
+        print("Username must have no spaces!")
+else:
+    print("Username must have no more than 12 characters!")
