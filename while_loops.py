36803af27979aa08bb63d28b7c9d638128c032b6
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-18 16:51:45 +0000

    add more practice using while loops

--- while_loops.py
+++ while_loops.py
@@ -1,32 +1,32 @@
-# # While Loops
+# While Loops
 
-# # asking for name
-# name = input("Please enter ur name: ")
+# asking for name
+name = input("Please enter ur name: ")
 
-# while name == "":
-#     print("You did not enter your name!")
-#     name = input("Please enter ur name: ")
+while name == "":
+    print("You did not enter your name!")
+    name = input("Please enter ur name: ")
 
-# print(f"Hello {name.title()}! 👋")
-
-
-
-# print(" --- next --- ")
+print(f"Hello {name.title()}! 👋")
 
 
 
-# # asking for age
-# age = input("Enter ur age: ")
-
-# while not age.isdigit():
-#     print("You did not enter a valid age!")
-#     age = input("Enter ur valid age: ")
-
-# print(f"You are {age}yrs old!")
+print(" --- next --- ")
 
 
 
-# print(" --- next --- ")
+# asking for age
+age = input("Enter ur age: ")
+
+while not age.isdigit():
+    print("You did not enter a valid age!")
+    age = input("Enter ur valid age: ")
+
+print(f"You are {age}yrs old!")
+
+
+
+print(" --- next --- ")
 
 
 
@@ -41,4 +41,26 @@ while entered_password != password or entered_username != username:
     print("You have entered the wrong username and/or password")
     entered_username = input("Please try again. Enter your username: ")
     entered_password = input("Please try again. Enter your password: ")
-    
+
+
+
+print(" --- next --- ")
+
+
+
+# logging in - broken down for more while loop practice (probs not good idea irl 🙃🤣😅😂😆😁)
+
+correct_name = "user3"
+correct_password = "abc123"
+
+login_name = input("Enter your login name: ")
+while not login_name == correct_name:
+    print("Wrong login name. Try again: ")
+    login_name = input("Enter your login name: ")
+
+login_password = input("Enter your login password: ")
+while not login_password == correct_password:
+        print("Wrong login password. Try again: ")
+        login_password = input("Enter your login password: ")
+
+print("passed") 
