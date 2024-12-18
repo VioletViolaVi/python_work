259c15d514cd2802dcb8fe4adc836c89df84e97b
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-18 15:34:23 +0000

    practice while loops from creating examples

--- while_loops.py
+++ while_loops.py
@@ -1,1 +1,33 @@
 # While Loops
+
+# asking for name
+name = input("Please enter ur name: ")
+
+while name == "":
+    print("You did not enter your name!")
+    name = input("Please enter ur name: ")
+
+print(f"Hello {name.title()}! 👋")
+
+
+
+print(" --- next --- ")
+
+
+
+# asking for age
+age = input("Enter ur age: ")
+
+while not age.isdigit():
+    print("You did not enter a valid age!")
+    age = input("Enter ur valid age: ")
+
+print(f"You are {age}yrs old!")
+
+
+
+print(" --- next --- ")
+
+
+
+#
