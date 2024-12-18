a96c69295271e81811623b94ab151bf9ec4fdf58
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-18 21:51:32 +0000

    practice more for loops

--- for_loops.py
+++ for_loops.py
@@ -1,4 +1,53 @@
 # For Loops
 
+foods = ["chips", "pizza", "chocolate", "cake"]
+for food in foods:
+    print(food)
+
+
+print(" --- next --- ")
+
+
+name = "vivian"
+for letter in name:
+    print(letter)
+
+
+print(" --- next --- ")
+
+
 for x in range(1, 11):
     print(x)
+
+
+print(" --- next --- ")
+
+
+for x in reversed(range(1, 11)):
+    print(x)
+
+
+print(" --- next --- ")
+
+
+counter = "12345"
+for count in counter:
+    print(count)
+print("GO!!!")
+
+
+print(" --- next --- ")
+
+
+for y in range(0, 11, 2):
+    print(y)
+
+
+print(" --- next --- ")
+
+
+for y in range(11):
+    print(y)
+
+
+print(" --- next --- ")
