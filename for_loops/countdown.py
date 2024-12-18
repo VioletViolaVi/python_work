96bf2c3de3cc317dbf2c89870b7e80cdae3ec4f6
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-18 23:21:16 +0000

    practice counting up & down using time.sleep() & for loops

--- for_loops/countdown.py
+++ for_loops/countdown.py
@@ -1,1 +1,15 @@
 # Countdown Exercise
+
+import time
+
+# count up
+for num in range(1, 6):    
+    print(num)
+    time.sleep(1)
+print("it's been 5 seconds")
+
+# count down
+for num in reversed(range(1, 6)):    
+    print(num)
+    time.sleep(1)
+print("happy new year! 🎆🎇🎆")
