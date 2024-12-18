060152b8b2d75b2465e7c79c232172516eaac768
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-18 00:14:36 +0000

    practice formatting using centre align

--- format_specifiers.py
+++ format_specifiers.py
@@ -6,7 +6,8 @@
     # {value:7} / {value:6} / {value:2} etc. -> adds spaces in front to total 7, 6 and 2 spaces in output
     # {value:07} / {value:06} / {value:02} etc. -> adds 0's instead of spaces in output
     # {value:<8} / {value:<4} / {value:<3} etc. -> aligns output to LEFT (space added ON END as needed)
-    # {value:<<8} / {value:<4} / {value:<3} etc. -> aligns output to RIGHT (space added AT FRONT as needed)
+    # {value:>8} / {value:>4} / {value:>3} etc. -> aligns output to RIGHT (space added AT FRONT as needed)
+    # {value:^22} etc. -> centers output (space added EITHER SIDE as needed)
     # 
     # #
 
@@ -19,37 +20,44 @@ print(f"Default chocolate price: £{chocolate_price}")
 print(f"Default ice cream price: £{ice_cream_price}")
 print(f"Default juice price: £{juice_price}")
 
+# print(" --- next ---")
+
+# # decimal places
+# print(f"9 decimal places: £{chocolate_price:.9f}")
+# print(f"2 decimal places: £{ice_cream_price:.2f}")
+# print(f"1 decimal place: £{juice_price:.1f}")
+
+# print(" --- next ---")
+
+# # adding spaces in front
+# print(f"20 spaces total: £{chocolate_price:20}")
+# print(f"21 spaces total: £{ice_cream_price:21}")
+# print(f"15 spaces total: £{juice_price:15}")
+
+# print(" --- next ---")
+
+# # adding 0's in front
+# print(f"20 spaces total: £{chocolate_price:020}")
+# print(f"21 spaces total: £{ice_cream_price:021}")
+# print(f"15 spaces total: £{juice_price:015}")
+
+# print(" --- next ---")
+
+# # left justify
+# print(f"left justified: £{chocolate_price:<20}")
+# print(f"left justified: £{ice_cream_price:<21}")
+# print(f"left justified: £{juice_price:<15}")
+
+# print(" --- next ---")
+
+# # right justify
+# print(f"right justified: £{chocolate_price:>20}")
+# print(f"right justified: £{ice_cream_price:>21}")
+# print(f"right justified: £{juice_price:>15}")
+
 print(" --- next ---")
 
-# decimal places
-print(f"9 decimal places: £{chocolate_price:.9f}")
-print(f"2 decimal places: £{ice_cream_price:.2f}")
-print(f"1 decimal place: £{juice_price:.1f}")
-
-print(" --- next ---")
-
-# adding spaces in front
-print(f"20 spaces total: £{chocolate_price:20}")
-print(f"21 spaces total: £{ice_cream_price:21}")
-print(f"15 spaces total: £{juice_price:15}")
-
-print(" --- next ---")
-
-# adding 0's in front
-print(f"20 spaces total: £{chocolate_price:020}")
-print(f"21 spaces total: £{ice_cream_price:021}")
-print(f"15 spaces total: £{juice_price:015}")
-
-print(" --- next ---")
-
-# left justify
-print(f"left justified: £{chocolate_price:<20}")
-print(f"left justified: £{ice_cream_price:<21}")
-print(f"left justified: £{juice_price:<15}")
-
-print(" --- next ---")
-
-# right justify
-print(f"right justified: £{chocolate_price:>20}")
-print(f"right justified: £{ice_cream_price:>21}")
-print(f"right justified: £{juice_price:>15}")
+# centre align
+print(f"centre aligned: £{chocolate_price:^22}")
+print(f"centre aligned: £{ice_cream_price:^22}")
+print(f"centre aligned: £{juice_price:^22}")
