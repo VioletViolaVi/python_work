e8196ee33be178de85ccb728b653b54bb0d73387
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-19 00:08:13 +0000

    make a started attempt at creating countdown clock

--- for_loops/countdown.py
+++ for_loops/countdown.py
@@ -2,20 +2,32 @@
 
 import time
 
-# count up
-for num in range(1, 6):    
-    print(num)
-    time.sleep(1)
-print("it's been 5 seconds")
+# # count up
+# for num in range(1, 6):    
+#     print(num)
+#     time.sleep(1)
+# print("it's been 5 seconds")
 
-# count down
-for num in reversed(range(1, 6)):    
-    print(num)
-    time.sleep(1)
-print("happy new year! 🎆🎇🎆")
+# # count down
+# for num in reversed(range(1, 6)):    
+#     print(num)
+#     time.sleep(1)
+# print("happy new year! 🎆🎇🎆")
 
-# opposite to above
-for num in range(5, 0, -1):    
-    print(num)
+# # opposite to above
+# for num in range(5, 0, -1):    
+#     print(num)
+#     time.sleep(1)
+# print("happy new year! 🎆🎇🎆")
+
+
+
+user_seconds = int(input("Enter a number of seconds: "))
+
+for second in range(user_seconds, 0, -1): # has been capped so change when done
+    secs = second % 60
+    mins = int(second / 60) % 60
+    hours = int(second / 3600)
     time.sleep(1)
-print("happy new year! 🎆🎇🎆")
+    print(f"{hours}:{mins}:{secs}") # start here next time to cont.
+print("done")
