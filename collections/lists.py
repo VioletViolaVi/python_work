df4165cdb9f4975f949016b0cee6e357f206ac9f
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-19 16:45:18 +0000

    print out lists

--- collections/lists.py
+++ collections/lists.py
@@ -5,3 +5,8 @@ foods = ["pizza", "burger", "pie", "chips", "soup"]
 weathers = ["sunny", "rainy", "clear"]
 animals = ["mouse", "dog", "fish", "cat", "donkey", "flamingo"]
 
+# lists printed out
+print(names)
+print(foods)
+print(weathers)
+print(animals)
