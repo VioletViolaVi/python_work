2ff7942d41d83ede11e26f864b0b0fa6a56d31b7
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-19 20:27:15 +0000

    add notes

--- collections/set.py
+++ collections/set.py
@@ -1,3 +1,9 @@
 # Sets
 
-
+# Notes:
+    # - sets 'print() out' differently each time '@ random'
+    # - sets cannot be edited (add/remove/reassign items) 
+    # - sets are unordered (probs 'cause/why they print out differently)
+    # - sets cannot have more than 1 of the same item (lists can)
+        # - {"eggs", "eggs", "bacon", "beans"} would NOT be allowed
+        # - .count() for counting multiple of the same item in a set would probs not be useful
