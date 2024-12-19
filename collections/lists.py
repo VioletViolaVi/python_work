f3ab8bf4667e51802887bd2ae9041922a3ed06d4
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-19 19:53:23 +0000

    use different list methods to practice manipulating lists

--- collections/lists.py
+++ collections/lists.py
@@ -48,3 +48,62 @@ print()
 for animal in animals:
     print(animal, end=", ")
 
+
+
+print(" --- next ---")
+
+
+
+# all diff. methods (using food python list)
+print(dir(foods))
+
+# index
+print(foods.index("pie"))
+
+# inset
+foods.insert(2,"bacon")
+print(foods)
+
+# pop
+foods.pop()
+print(foods)
+foods.pop(2)
+print(foods)
+
+# remove
+foods.remove("pizza")
+print(foods)
+
+# reverse
+foods.reverse()
+print(foods)
+
+# sort
+foods.sort()
+print(foods)
+foods.sort(reverse=True)
+print(foods)
+
+# append -> adds ONE item/elements
+foods.append(["cake", "chocolate"])
+foods.append("turkey")
+for food in foods:
+    print(food)
+print(foods)
+
+# extend -> adds multiple items/elements
+foods.extend("soup") # - adds 's', 'o', 'u', 'p' NOT soup
+foods.extend(["chicken", "jelly"])
+print(foods)
+
+# count: number of times item/element appears in list
+print(foods.count("soup"))
+
+# copy
+foods.copy()
+for food in foods:
+    print(food)
+
+# remove all items/elements
+foods.clear()
+print(foods)
