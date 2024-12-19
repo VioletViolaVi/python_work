dc36d0baa9892387f99e04cab00f4ad7e2455910
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-19 16:42:23 +0000

    add different python lists examples

--- collections/lists.py
+++ collections/lists.py
@@ -1,2 +1,7 @@
 # Lists
 
+names = ["vivian", "bella", "steve", "mike", "traci", "duke"]
+foods = ["pizza", "burger", "pie", "chips", "soup"]
+weathers = ["sunny", "rainy", "clear"]
+animals = ["mouse", "dog", "fish", "cat", "donkey", "flamingo"]
+
