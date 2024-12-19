2102241cc6c59490dfb58c0ee155e1c0c314ef23
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-19 16:23:45 +0000

    create rectangle using nested loops

--- loops/nested_loops.py
+++ loops/nested_loops.py
@@ -26,3 +26,20 @@ for x in range(0, 3):
     for y in range(0, 10):
         print(y, end="")
     print()
+
+
+
+print(" --- new ---")
+
+
+
+# rectangle
+user_rows = int(input("How many rows?: "))
+user_cols = int(input("How many columns?: "))
+sign = input("Pick a sign: ")
+
+print("Here is your rectangle:")
+for row in range(0, user_rows):
+    for col in range(0, user_cols):
+        print(sign, end="")
+    print()
