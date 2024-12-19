d5591b56bc8719031fb1c58f623a9f5b553232ca
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-19 16:12:34 +0000

    add more nested loop practice

--- loops/nested_loops.py
+++ loops/nested_loops.py
@@ -4,3 +4,25 @@ for outer in range(0, 3):
     for inner in range(0, 3):   
         print("inner", end=" ")        
     print("outer")
+
+
+
+print(" --- new ---")
+
+
+
+# from vid
+for x in range(0, 10):
+    print(x, end="")
+print()
+
+# print 3x
+for x in range(0, 10):
+    for y in range(0, 3):
+        print(y, end="")
+
+# print 3x on own lines
+for x in range(0, 3):
+    for y in range(0, 10):
+        print(y, end="")
+    print()
