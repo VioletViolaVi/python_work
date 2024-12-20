f13467d7540565e93d09ed31c25fc3978960cd47
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 17:05:01 +0000

    practice reassigning items in 2D list

--- collections/2d_lists.py
+++ collections/2d_lists.py
@@ -1,8 +1,15 @@
-# 2D Lists
+# 2D (Dimensional) Lists
 
 desserts = ["jelly", "ice-cream", "custard", "cake", "muffins", "brownies"]
 mains = ["Vine Tomato & Basil Soup", "Pan-fried Scallops", "New Potato Salad", "Braised Beef Cheek", "Butternut Squash & Pepper Curry"]
 drinks = ["water", "juice", "coffee", "tea", "lemonade", "milkshake", "smoothie"]
 
-restaurant_meal = [desserts, mains, drinks]
-print(f"restaurant_meal: {restaurant_meal}")
+# 2d list
+restaurant = [desserts, mains, drinks]
+
+print(f"restaurant: {restaurant}")
+print(restaurant[2][4])
+
+# change 1st item in 1st list
+restaurant[0][0] = "Raspberry and Pistachio Mango Mousse"
+print(restaurant[0])
