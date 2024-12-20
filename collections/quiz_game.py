af64dba5700515f4bf6b0bb8e7bb652b23d98bdb
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 19:19:49 +0000

    fill in options

--- collections/quiz_game.py
+++ collections/quiz_game.py
@@ -4,20 +4,20 @@
     # https://parade.com/944584/parade/trivia-questions-for-kids/
 
 # questions
-ques_1 = "In the nursery rhyme, who sat on a wall before having a great fall?"
+ques_1 = "In the nursery rhyme, who had a farm?"
 ques_2 = "If you freeze water, what do you get?"
 ques_3 = "How many legs does a spider have?"
 ques_4 = "Where does Santa Claus live?"
 ques_5 = "On which holiday do you go trick-or-treating?"
 
 # options
-ques_1_options = ("A)#", "B)#", "C)#", "D)#")
-ques_2_options = ("A)#", "B)#", "C)#", "D)#")
-ques_3_options = ("A)#", "B)#", "C)#", "D)#")
-ques_4_options = ("A)#", "B)#", "C)#", "D)#")
-ques_5_options = ("A)#", "B)#", "C)#", "D)#")
+ques_1_options = ("A) Little Bo Peep", "B) Old MacDonald", "C) The Muffin Man", "D) Jack and Jill")
+ques_2_options = ("A) Ice", "B) Lava", "C) Fire", "D) Wind")
+ques_3_options = ("A) Six", "B) Four", "C) Eight", "D)Zero")
+ques_4_options = ("A) Loc Ness", "B) The North Pole", "C) Atlantis", "D) Townsville")
+ques_5_options = ("A) Christmas", "B) Easter", "C) Valentines Day", "D) Halloween")
 
+# nested tuples using above values
 questions = (ques_1, ques_2, ques_3, ques_4, ques_5)
 all_options = (ques_1_options, ques_2_options, ques_3_options, ques_4_options, ques_5_options)
-
 
