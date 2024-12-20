8860aa9e52c0ac214285d462845de53914ca5c06
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 18:36:31 +0000

    recreate keypad using tuples

--- collections/keypad.py
+++ collections/keypad.py
@@ -6,6 +6,7 @@
     # 7 8 9
     # * 0 #
 
+# LISTS
 # create row lists
 row_1 = ["1", "2", "3"]
 row_2 = ["4", "5", "6"]
@@ -21,3 +22,22 @@ for row in keypad:
         print(btn, end=" ")
     print()
 
+print()
+print(" --- list above tuple below --- ")
+print()
+
+# TUPLES
+# create row tuples
+row_1 = ("1", "2", "3")
+row_2 = ("4", "5", "6")
+row_3 = ("7", "8", "9")
+row_4 = ("*", "0", "#")
+
+# create tuples containing all rows
+keypad = (row_1, row_2, row_3, row_4)
+
+# print individually
+for row in keypad:
+    for btn in row:
+        print(btn, end=" ")
+    print()
