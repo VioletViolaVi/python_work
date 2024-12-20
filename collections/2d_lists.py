4891b6b160e4d1681d10676b458c3c2ed7ab03d6
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 17:31:24 +0000

    practice printing out items from 2D lists using nested for loops

--- collections/2d_lists.py
+++ collections/2d_lists.py
@@ -6,6 +6,10 @@ drinks = ["water", "juice", "coffee", "tea", "lemonade", "milkshake", "smoothie"
 
 # 2d list
 restaurant = [desserts, mains, drinks]
+# same as above
+# restaurant = [["jelly", "ice-cream", "custard", "cake", "muffins", "brownies"], 
+#               ["Vine Tomato & Basil Soup", "Pan-fried Scallops", "New Potato Salad", "Braised Beef Cheek", "Butternut Squash & Pepper Curry"], 
+#               ["water", "juice", "coffee", "tea", "lemonade", "milkshake", "smoothie"]]
 
 print(f"restaurant: {restaurant}")
 print(restaurant[2][4])
@@ -13,3 +17,21 @@ print(restaurant[2][4])
 # change 1st item in 1st list
 restaurant[0][0] = "Raspberry and Pistachio Mango Mousse"
 print(restaurant[0])
+
+
+
+print(" --- new --- ")
+
+
+
+# for loops / nested 
+print()
+for restaurant_menu in restaurant:
+    print(restaurant_menu)
+    # goes after individual menu items
+    for restaurant_item in restaurant_menu:
+        print(restaurant_item)
+
+
+
+
