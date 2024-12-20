6010cdcf4ef511cea14f1c8100e48a99e46cfc96
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 15:08:36 +0000

    add practice with while loop & append method

--- collections/shopping_cart.py
+++ collections/shopping_cart.py
@@ -3,3 +3,12 @@
 foods = []
 prices = []
 total = 0
+
+print(f"start: {foods}")
+
+user_request = input("What food would you like to buy? ('x' to quit): ").lower()
+
+while not user_request == "x":
+    foods.append(user_request)
+    user_request = input("What food would you like to buy? ('x' to quit): ").lower()
+print(f"end: {foods}")
