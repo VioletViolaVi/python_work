5acb3865ed693a5a49518e4146f6d396c1b1765d
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 19:05:14 +0000

    create tuples for question options

--- collections/quiz_game.py
+++ collections/quiz_game.py
@@ -4,10 +4,20 @@
     # https://parade.com/944584/parade/trivia-questions-for-kids/
 
 # questions
-ques_1 = "What is the name of the fairy in Peter Pan?"
+ques_1 = "In the nursery rhyme, who sat on a wall before having a great fall?"
 ques_2 = "If you freeze water, what do you get?"
 ques_3 = "How many legs does a spider have?"
 ques_4 = "Where does Santa Claus live?"
 ques_5 = "On which holiday do you go trick-or-treating?"
 
+# options
+ques_1_options = ("A)#", "B)#", "C)#", "D)#")
+ques_2_options = ("A)#", "B)#", "C)#", "D)#")
+ques_3_options = ("A)#", "B)#", "C)#", "D)#")
+ques_4_options = ("A)#", "B)#", "C)#", "D)#")
+ques_5_options = ("A)#", "B)#", "C)#", "D)#")
+
 questions = (ques_1, ques_2, ques_3, ques_4, ques_5)
+all_options = (ques_1_options, ques_2_options, ques_3_options, ques_4_options, ques_5_options)
+
+
