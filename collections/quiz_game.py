613e217efd74298c55e25854a726bcf6c5cd0e86
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 18:56:58 +0000

    add questions & create tuple

--- collections/quiz_game.py
+++ collections/quiz_game.py
@@ -3,3 +3,11 @@
 # Questions taken from:
     # https://parade.com/944584/parade/trivia-questions-for-kids/
 
+# questions
+ques_1 = "What is the name of the fairy in Peter Pan?"
+ques_2 = "If you freeze water, what do you get?"
+ques_3 = "How many legs does a spider have?"
+ques_4 = "Where does Santa Claus live?"
+ques_5 = "On which holiday do you go trick-or-treating?"
+
+questions = (ques_1, ques_2, ques_3, ques_4, ques_5)
