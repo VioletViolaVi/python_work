df42cd978e97902e245c6f3e00d2f43c0376ae12
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 16:40:11 +0000

    add 3 lists

--- collections/2d_lists.py
+++ collections/2d_lists.py
@@ -1,3 +1,5 @@
 # 2D Lists
 
-
+desserts = ["jelly", "ice-cream", "custard", "cake", "muffins", "brownies"]
+drinks = ["water", "juice", "coffee", "tea", "lemonade", "milkshake", "smoothie"]
+furniture = ["sofa", "table", "bed", "armchair", "cupboard", "drawers", "tv"]
