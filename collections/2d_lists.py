e60425e438bf9ddb0286cc68e69113855dbbb40a
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 16:53:29 +0000

    create & test 2D list

--- collections/2d_lists.py
+++ collections/2d_lists.py
@@ -4,4 +4,5 @@ desserts = ["jelly", "ice-cream", "custard", "cake", "muffins", "brownies"]
 mains = ["Vine Tomato & Basil Soup", "Pan-fried Scallops", "New Potato Salad", "Braised Beef Cheek", "Butternut Squash & Pepper Curry"]
 drinks = ["water", "juice", "coffee", "tea", "lemonade", "milkshake", "smoothie"]
 
-
+restaurant_meal = [desserts, mains, drinks]
+print(f"restaurant_meal: {restaurant_meal}")
