a7ead7a904b075284e7a9e357f03a7697c8326f2
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-20 18:24:54 +0000

    print items individually following keypad format

--- collections/keypad.py
+++ collections/keypad.py
@@ -6,10 +6,18 @@
     # 7 8 9
     # * 0 #
 
+# create row lists
 row_1 = ["1", "2", "3"]
 row_2 = ["4", "5", "6"]
 row_3 = ["7", "8", "9"]
 row_4 = ["*", "0", "#"]
 
+# create list containing all rows
 keypad = [row_1, row_2, row_3, row_4]
+
+# print individually
+for row in keypad:
+    for btn in row:
+        print(btn, end=" ")
+    print()
 
