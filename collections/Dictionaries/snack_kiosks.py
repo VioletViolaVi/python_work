959df322afaa50865994f16efa46f76770d1b7c6
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-21 20:10:49 +0000

    edit dictionary

--- collections/Dictionaries/snack_kiosks.py
+++ collections/Dictionaries/snack_kiosks.py
@@ -3,8 +3,11 @@
 # food keys, price values
 snack_bar_menu = {
     "popcorn" : 1.99,
-    "water" : 0.75,
     "hot dogs" : 7.05,
-    "sweets" : 4.28,
-    "apple tango" : 6.19,
+    "chips" : 4.28,
+    "chicken nuggets" : 8.10,
+    "water" : 0.75,
+    "strawberry milkshake" : 6.19,
+    "banana smoothie" : 2.00,
+    "lemonade" : 3.03,
 }
