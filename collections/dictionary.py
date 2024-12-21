4c9cbf88e2638fa41bada1c73f6ee88ea2fb3523
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-21 17:56:26 +0000

    practice using .get() method to retrieve values from keys, if present

--- collections/dictionary.py
+++ collections/dictionary.py
@@ -1,5 +1,6 @@
 # Dictionaries
 
+# key:value
 country_capitals = {
     "United Kingdom": "London",
     "Japan": "Tokyo",
@@ -7,6 +8,23 @@ country_capitals = {
     "Ghana": "Accra",
 }
 
-# get assistance
-print(dir(country_capitals))
-print(help(country_capitals))
+# retrieves value from key in dictionary
+print(country_capitals.get("China"))
+print(country_capitals.get("USA")) # 'None' returned if no key
+
+
+
+print(" --- next --- ")
+
+
+
+# conditional statements regarding presence of keys
+location = country_capitals.get("USA")
+if location == None:
+    print("This country is not a key")
+else:
+    print(f"Hello {location}! 👋")
+
+# # get assistance
+# print(dir(country_capitals))
+# print(help(country_capitals))
