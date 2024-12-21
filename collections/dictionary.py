6ba9143544360c9d7785ef516ed70ea2a0489212
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-21 17:47:24 +0000

    create first dictionary

--- collections/dictionary.py
+++ collections/dictionary.py
@@ -1,3 +1,12 @@
 # Dictionaries
 
+country_capitals = {
+    "United Kingdom": "London",
+    "Japan": "Tokyo",
+    "China": "Beijing",
+    "Ghana": "Accra",
+}
 
+# get assistance
+print(dir(country_capitals))
+print(help(country_capitals))
