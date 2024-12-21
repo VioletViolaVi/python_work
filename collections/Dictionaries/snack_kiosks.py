5f5824ef5248adc97bc6c5674d9d7fd3781b8a46
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-21 20:05:01 +0000

    create dictionary for foods and prices

--- collections/Dictionaries/snack_kiosks.py
+++ collections/Dictionaries/snack_kiosks.py
@@ -1,3 +1,10 @@
 #  Snack Kiosks / Snack Bars
 
-
+# food keys, price values
+snack_bar_menu = {
+    "popcorn" : 1.99,
+    "water" : 0.75,
+    "hot dogs" : 7.05,
+    "sweets" : 4.28,
+    "apple tango" : 6.19,
+}
