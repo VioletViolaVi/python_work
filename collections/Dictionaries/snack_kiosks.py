38069b3e9ab92509440db5a7b0788de09a078c58
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-21 22:47:10 +0000

    attempt to prevent users from entering snack item not in dictionary

--- collections/Dictionaries/snack_kiosks.py
+++ collections/Dictionaries/snack_kiosks.py
@@ -31,20 +31,32 @@ if user_request == "c".lower():
         print(f"£{price:.2f}")
     print()
 
-    while not user_request == "x".lower():        
-        user_request = input("What would you like to buy? Select from above menu ('x' to quit): ").lower() # asks user what they want       
-        basket.append(user_request) # puts what they want in basket        
-        prices.append(snack_menu.get(user_request)) # get respective prices stored
+    # purchasing snacks
+    while not user_request == "x".lower():
+        # asks user what they want 
+        user_request = input("Select from above menu what you want to buy('x' to quit): ").lower()  
+        # user enters item not on menu
+        if snack_menu.get(user_request) == None:
+            user_request = input("Sorry. That's not available. Select from above menu what you want to buy('x' to quit): ").lower()
+        else:
+            # asks user what they want 
+            user_request = input("Select from above menu what you want to buy('x' to quit): ").lower()
+            # puts what they want in basket        
+            basket.append(user_request)
+            # get respective snack prices stored
+            prices.append(snack_menu.get(user_request))
 
     basket.pop() # removes 'x' from list when user stops shopping
     prices.pop() # removes 'None' from list when user stops shopping
+    
+    # adds all prices
+    for price in prices:
+        total += price
+        print(f"price: {price}")
+        print(f"total: {total}")
 
-print(f"prices: {prices}")
-# adds up all prices
-for price in prices:
-    total += price
-    print(f"price: {price}")
-    print(f"total: {total}")
+print(f"basket: {basket}") # delete later
+print(f"prices: {prices}") # delete later
 
 # display purchases & cost
 print(" ------ Checkout ------  ")
