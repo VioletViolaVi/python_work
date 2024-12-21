383f1ff4087172502fc5e1af89b9c0c2789f96c2
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-21 18:08:31 +0000

    practice adding value with keyname, when key is present or not, to study output

--- collections/dictionary.py
+++ collections/dictionary.py
@@ -5,12 +5,15 @@ country_capitals = {
     "United Kingdom": "London",
     "Japan": "Tokyo",
     "China": "Beijing",
-    "Ghana": "Accra",
+    "Ghana": "Accra"
 }
 
 # retrieves value from key in dictionary
-print(country_capitals.get("China"))
-print(country_capitals.get("USA")) # 'None' returned if no key
+print(country_capitals.get("China")) # 'Beijing' returned, key is present
+print(country_capitals.get("USA")) # 'None' (default) returned, there's no 'USA' key
+print(country_capitals.get("Ghana", "Absent")) # 'Accra' returned, key is present
+print(country_capitals.get("Singapore", "Absent")) # 'Absent' (custom) returned, there's no 'Singapore' key
+print(country_capitals.get("New Zealand", "Not a key")) # 'Not a key' returned, there's no 'New Zealand' key
 
 
 
@@ -24,6 +27,15 @@ if location == None:
     print("This country is not a key")
 else:
     print(f"Hello {location}! 👋")
+
+
+
+print(" --- next --- ")
+
+
+
+
+
 
 # # get assistance
 # print(dir(country_capitals))
