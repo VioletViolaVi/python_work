29e7301ae46ced46733c6d0290de68b31fcc693c
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-21 21:38:15 +0000

    add code to add users snack choices to list

--- collections/Dictionaries/snack_kiosks.py
+++ collections/Dictionaries/snack_kiosks.py
@@ -5,12 +5,12 @@
 # food keys, price values
 snack_menu = {
     "popcorn" : 1.99,
-    "hot dogs" : 7.05,
     "chips" : 4.28,
+    "hot dogs" : 7.05,
     "chicken nuggets" : 8.10,
-    "water" : 0.75,
     "strawberry milkshake" : 6.19,
     "banana smoothie" : 2.00,
+    "water" : 0.75,
     "lemonade" : 3.03,
 }
 
@@ -21,17 +21,21 @@ total = 0
 
 # user enters what they want
 user_request = input("Welcome to the snack bar. Press 'C' to continue: ").lower()
-print(user_request)
 
-while user_request == "c":
+if user_request == "c".lower():
     # displays snack menu
+    print(" Snack   Price   ")
+    print(" ------  ------  ")
     for snack, price in snack_menu.items():
-        print(" --- --- ")
-        print(f"Snack: {snack.title()}", end=", ")
-        print(f"Price: £{price:.2f}")
+        print(f"{snack.title()}", end=",  ")
+        print(f"£{price:.2f}")
     print()
 
-    # asks user what they want
-    user_request = input("What would you like to buy? Select from above menu ('x' to quit): ").lower()
-    # puts what they want in basket
-    basket = basket.append(user_request)
+    while not user_request == "x".lower():
+        # asks user what they want
+        user_request = input("What would you like to buy? Select from above menu ('x' to quit): ").lower()
+        # puts what they want in basket
+        basket.append(user_request)
+    basket.pop() # removes 'x' from list when user stops shopping
+
+print(f"basket: {basket}")
