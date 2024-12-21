2209b109f530c30d62fccc251deb10f5e4d4c62b
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-21 23:43:17 +0000

    attempt to fix exiting bug & add code to prevent .pop() error on empty lists

--- collections/Dictionaries/snack_kiosks.py
+++ collections/Dictionaries/snack_kiosks.py
@@ -24,6 +24,7 @@ user_request = input("Welcome to the snack bar. Press 'C' to continue: ").lower(
 
 if user_request == "c".lower():
     # displays snack menu
+    print("-------- --------")
     print("  Snack   Price  ")
     print("-------- --------")
     for snack, price in snack_menu.items():
@@ -31,23 +32,30 @@ if user_request == "c".lower():
         print(f"£{price:.2f}")
     print()
 
+    # asks user what they want 
+    user_request = input("A) Select from above menu what you want to buy ('x' to quit): ").lower()
+
     # purchasing snacks
     while not user_request == "x".lower():
-        # asks user what they want 
-        user_request = input("Select from above menu what you want to buy('x' to quit): ").lower()  
+        # # asks user what they want 
+        # user_request = input("A) Select from above menu what you want to buy ('x' to quit): ").lower()
         # user enters item not on menu
         if snack_menu.get(user_request) == None:
-            user_request = input("Sorry. That's not available. Select from above menu what you want to buy('x' to quit): ").lower()
+            user_request = input("B) Sorry, that's not available. Select from above menu what you want to buy ('x' to quit): ").lower()
+            while snack_menu.get(user_request) == None: # i.e. user is STILL entering an item not in the dictionary at this point
+                user_request = input("C) Sorry, that's not available. Select from above menu what you want to buy ('x' to quit): ").lower()
         else:
             # asks user what they want 
-            user_request = input("Select from above menu what you want to buy('x' to quit): ").lower()
+            user_request = input("D) Select from above menu what you want to buy ('x' to quit): ").lower()
             # puts what they want in basket        
             basket.append(user_request)
             # get respective snack prices stored
             prices.append(snack_menu.get(user_request))
 
-    basket.pop() # removes 'x' from list when user stops shopping
-    prices.pop() # removes 'None' from list when user stops shopping
+    # stops .pop() on empty lists, only 'pops' on lists with items inside
+    if not basket == [] and not prices == []:
+        basket.pop() # removes 'x' from list when user stops shopping
+        prices.pop() # removes 'None' from list when user stops shopping
     
     # adds all prices
     for price in prices:
@@ -65,3 +73,5 @@ if basket == [] and total == 0: # user fails to enter 'c'.lower()
 else:
     print(f"Your basket: {basket}")
     print(f"Your Total: £{round(total, 2)}") # avoids long decimal numbers
+
+# - ensure u can quit at any stage!
