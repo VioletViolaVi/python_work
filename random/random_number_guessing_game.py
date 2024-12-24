5385bc90dc3b096ca34fe05582c8384ccea91037
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-24 21:52:49 +0000

    remove print() statements

--- random/random_number_guessing_game.py
+++ random/random_number_guessing_game.py
@@ -4,7 +4,6 @@ import random
 
 # random number to guess
 correct_num = random.randint(1, 5) # both numbers (1, 5) are included!!!
-print(f"0) correct_num: {correct_num}")
 
 # get number guess from user between 1 and 5
 user_guess = int(input("Guess the number between 1 and 5 (inclusively): "))
@@ -12,10 +11,6 @@ user_guess = int(input("Guess the number between 1 and 5 (inclusively): "))
 # prevent number being out of range of 1 and 5
 while user_guess < 1 or user_guess > 5:
     user_guess = int(input("Number out of range! Guess the number between 1 and 5 (inclusively): "))
-
-
-print(f"1) user_guess: {user_guess}")
-print(f"1) correct_num: {correct_num}")
 
 # user keeps trying to guess correct num
 while not user_guess == correct_num:
@@ -32,11 +27,6 @@ while not user_guess == correct_num:
     else:
         print("Error! 🫠")
 
-    print(f"2) user_guess: {user_guess}")
-    print(f"2) correct_num: {correct_num}")
-
 # user guesses correct number
 if user_guess == correct_num:
     print("Well done! You guessed correctly! 🥳")
-    print(f"3) user_guess: {user_guess}")
-    print(f"3) correct_num: {correct_num}")
