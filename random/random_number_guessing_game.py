d9306ca829917e47feba566faf86f51082a86083
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-24 21:06:41 +0000

    create conditional while loop to prevent user guess being out of range

--- random/random_number_guessing_game.py
+++ random/random_number_guessing_game.py
@@ -8,6 +8,9 @@ correct_num = random.randint(1, 6)
 # get number guess from user between 1 and 5
 user_guess = int(input("Guess the number between 1 and 5 (inclusively): "))
 
+# prevent number being out of range of 1 and 5
+while user_guess < 1 or user_guess > 5:
+    user_guess = int(input("Number out of range! Guess the number between 1 and 5 (inclusively): "))
 
 print(f"user_guess: {user_guess}")
 print(f"correct_num: {correct_num}")
