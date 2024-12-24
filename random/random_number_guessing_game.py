a0d6f6c163dc6c83c12fd811ab3a124bb020d227
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-24 22:02:20 +0000

    create int variables for start & end of random number range then format into code so project is more dynamic

--- random/random_number_guessing_game.py
+++ random/random_number_guessing_game.py
@@ -2,28 +2,33 @@
 
 import random
 
+# numbers for guess range
+start_range = 1
+end_range = 10
+
 # random number to guess
-correct_num = random.randint(1, 5) # both numbers (1, 5) are included!!!
+correct_num = random.randint(start_range, end_range) # both numbers i.e. 'start_range' and 'end_range' are included in range!!!
+print(f"correct_num: {correct_num}")
 
-# get number guess from user between 1 and 5
-user_guess = int(input("Guess the number between 1 and 5 (inclusively): "))
+# get number guess from user between 'start_range' and 'end_range'
+user_guess = int(input(f"Guess the number between {start_range} and {end_range} (inclusively): "))
 
-# prevent number being out of range of 1 and 5
-while user_guess < 1 or user_guess > 5:
-    user_guess = int(input("Number out of range! Guess the number between 1 and 5 (inclusively): "))
+# prevent number being out of range of 'start_range' and 'end_range'
+while user_guess < start_range or user_guess > end_range:
+    user_guess = int(input(f"Number out of range! Guess the number between {start_range} and {end_range} (inclusively): "))
 
 # user keeps trying to guess correct num
 while not user_guess == correct_num:
 
-    # prevent number being out of range of 1 and 5 after at least 1 guess made
-    while user_guess < 1 or user_guess > 5:
-        user_guess = int(input("Not accepted! Out of range! Guess the number between 1 and 5 (inclusively): "))
+    # prevent number being out of range of {start_range} and {end_range} after at least 1 guess made
+    while user_guess < start_range or user_guess > end_range:
+        user_guess = int(input(f"Not accepted! Out of range! Guess the number between {start_range} and {end_range} (inclusively): "))
 
     # informing user how far off they're WRONG guess is
     if user_guess > correct_num:
-        user_guess = int(input("It's lower than that! Guess the number between 1 and 5 (inclusively): "))
+        user_guess = int(input(f"It's lower than that! Guess the number between {start_range} and {end_range} (inclusively): "))
     elif user_guess < correct_num:
-        user_guess = int(input("It's higher than that! Guess the number between 1 and 5 (inclusively): "))
+        user_guess = int(input(f"It's higher than that! Guess the number between {start_range} and {end_range} (inclusively): "))
     else:
         print("Error! 🫠")
 
