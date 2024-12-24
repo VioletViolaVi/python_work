fe651ada627b7cf83464de41ae3c22b2e3b520c5
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-24 21:06:13 +0000

    create code to get user number guess

--- random/random_number_guessing_game.py
+++ random/random_number_guessing_game.py
@@ -5,4 +5,9 @@ import random
 # random number to guess
 correct_num = random.randint(1, 6)
 
+# get number guess from user between 1 and 5
+user_guess = int(input("Guess the number between 1 and 5 (inclusively): "))
+
+
+print(f"user_guess: {user_guess}")
 print(f"correct_num: {correct_num}")
