4aae265130b598354ab76e1f5f102814975a9ff4
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-24 20:51:18 +0000

    practice shuffling list using shuffle()

--- random_numbers.py
+++ random_numbers.py
@@ -98,78 +98,91 @@ print("========= new ========= ")
 print()
 
 
-# user vs computer rock, paper, scissors game
-all_options = ["rock", "paper", "scissors"]
+# # user vs computer rock, paper, scissors game
+# all_options = ["rock", "paper", "scissors"]
 
-# keeping score
-user_score = 0
-computer_score = 0
+# # keeping score
+# user_score = 0
+# computer_score = 0
 
-# start num for while loop
-counter = 0
+# # start num for while loop
+# counter = 0
 
-# plays game 5 times
-while counter < 5:
+# # plays game 5 times
+# while counter < 5:
 
-    # gets choices made from user and computer
-    user_choice = input("choose: rock, paper or scissors: ").lower()
+#     # gets choices made from user and computer
+#     user_choice = input("choose: rock, paper or scissors: ").lower()
     
-    # to only accept rock, paper or scissors
-    while not user_choice == "rock" and not user_choice == "paper" and not user_choice == "scissors":
-            user_choice = input("you can only choose from: rock, paper or scissors: ").lower()
+#     # to only accept rock, paper or scissors
+#     while not user_choice == "rock" and not user_choice == "paper" and not user_choice == "scissors":
+#             user_choice = input("you can only choose from: rock, paper or scissors: ").lower()
 
-    # picks rock, paper or scissors from list
-    computer_choice = random.choice(all_options)
+#     # picks rock, paper or scissors from list
+#     computer_choice = random.choice(all_options)
 
-    if user_choice == computer_choice:
-        print("DRAW!")
+#     if user_choice == computer_choice:
+#         print("DRAW!")
 
-    elif user_choice == "rock":
-        if computer_choice == "scissors":
-            print("you win! rock beats scissors!")
-            user_score += 1
-        else:
-            print("you lose! paper beats rock!")
-            computer_score += 1
+#     elif user_choice == "rock":
+#         if computer_choice == "scissors":
+#             print("you win! rock beats scissors!")
+#             user_score += 1
+#         else:
+#             print("you lose! paper beats rock!")
+#             computer_score += 1
 
-    elif user_choice == "paper":
-        if computer_choice == "rock":
-            print("you win! paper beats rock!")
-            user_score += 1
-        else:
-            print("you lose! scissors beats paper!")
-            computer_score += 1
+#     elif user_choice == "paper":
+#         if computer_choice == "rock":
+#             print("you win! paper beats rock!")
+#             user_score += 1
+#         else:
+#             print("you lose! scissors beats paper!")
+#             computer_score += 1
 
-    elif user_choice == "scissors":
-        if computer_choice == "paper":
-            print("you win! scissors beats paper!")
-            user_score += 1
-        else:
-            print("you lose! rock beats scissors!")
-            computer_score += 1
+#     elif user_choice == "scissors":
+#         if computer_choice == "paper":
+#             print("you win! scissors beats paper!")
+#             user_score += 1
+#         else:
+#             print("you lose! rock beats scissors!")
+#             computer_score += 1
 
-    else:
-        print("error 😶")
+#     else:
+#         print("error 😶")
 
-    # increase counter to make while loop statement false after 5 iterations
-    counter += 1
+#     # increase counter to make while loop statement false after 5 iterations
+#     counter += 1
 
-print("------")
-print("Scores")
-print(f"your score: {user_score} computer's score: {computer_score}")
+# print("------")
+# print("Scores")
+# print(f"your score: {user_score} computer's score: {computer_score}")
 
-# stores bigger score
-higher_score = max(user_score, computer_score)
+# # stores bigger score
+# higher_score = max(user_score, computer_score)
 
-# states who wins
-if higher_score == user_score and higher_score == computer_score:
-    print(f"it's a tie!")
+# # states who wins
+# if higher_score == user_score and higher_score == computer_score:
+#     print(f"it's a tie!")
 
-elif higher_score == user_score:
-    print(f"you are the winner!")
+# elif higher_score == user_score:
+#     print(f"you are the winner!")
 
-elif higher_score == computer_score:
-    print(f"computer is the winner!")
+# elif higher_score == computer_score:
+#     print(f"computer is the winner!")
 
-else:
-    print("error 😶")
+# else:
+#     print("error 😶")
+
+
+print()
+print("========= new ========= ")
+print()
+
+
+# practicing shuffling
+str_nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+shuffled_str_nums = random.shuffle(str_nums)
+# shuffled_str_nums will return 'None'
+print(f"shuffled_str_nums: {shuffled_str_nums}")
+print(f"str_nums: {str_nums}")
