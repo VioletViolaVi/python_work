bbd521d30aa26b6aad6fae4bf77d66d8199ecf93
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-24 23:23:23 +0000

    create code so users can change game difficulty

--- random/random_number_guessing_game.py
+++ random/random_number_guessing_game.py
@@ -5,37 +5,60 @@ import random
 # ends game
 done = False
 
-# numbers for guess range
+# start number for guess range
 start_range = 1
-end_range = 100
 
-# random number to guess
-correct_num = random.randint(start_range, end_range) # both numbers i.e. 'start_range' and 'end_range' are included in range!!!
+# varied end number for game difficulty
+official_end_range = 0
+easy_end_range = 10
+mid_end_range = 50
+hard_end_range = 100
 
 while not done:
-    # get number guess from user between 'start_range' and 'end_range'
-    print("--------------------------------------")
-    user_guess = int(input(f"Guess the number between {start_range} and {end_range} (inclusively): "))
+    # user chooses game difficulty
+    user_difficulty = input(f"Choose level difficulty - easy (E), medium (M), hard (H): ").lower()
     print("--------------------------------------")
 
-    # prevent number being out of range of 'start_range' and 'end_range'
-    while user_guess < start_range or user_guess > end_range:
-        user_guess = int(input(f"Number out of range! Guess the number between {start_range} and {end_range} (inclusively): "))
+    # prevents anything but "e", "m" or "h" being accepted as level difficulty
+    while not user_difficulty == "e" and not user_difficulty == "m" and not user_difficulty == "h":
+        user_difficulty = input("Choose 'E', 'M' or 'H' only. Choose level difficulty - easy (E), medium (M), hard (H): ").lower()
+        print("--------------------------------------")
+    
+    # change game difficulty
+    if user_difficulty == "e":
+        official_end_range = easy_end_range
+    elif user_difficulty == "m":
+        official_end_range = mid_end_range
+    elif user_difficulty == "h":
+        official_end_range = hard_end_range
+
+    # random number to guess
+    # both numbers i.e. 'start_range' and 'official_end_range' are included in range!!!
+    correct_num = random.randint(start_range, official_end_range)
+
+    # get number guess from user between 'start_range' and 'official_end_range'
+    print("--------------------------------------")
+    user_guess = int(input(f"Guess the number between {start_range} and {official_end_range} (inclusively): "))
+    print("--------------------------------------")
+
+    # prevent number being out of range of 'start_range' and 'official_end_range'
+    while user_guess < start_range or user_guess > official_end_range:
+        user_guess = int(input(f"Number out of range! Guess the number between {start_range} and {official_end_range} (inclusively): "))
 
     # user keeps trying to guess correct num
     while not user_guess == correct_num:
 
-        # prevent number being out of range of {start_range} and {end_range} after at least 1 guess made
-        while user_guess < start_range or user_guess > end_range:
-            user_guess = int(input(f"Not accepted! Out of range! Guess the number between {start_range} and {end_range} (inclusively): "))
+        # prevent number being out of range of {start_range} and {official_end_range} after at least 1 guess made
+        while user_guess < start_range or user_guess > official_end_range:
+            user_guess = int(input(f"Not accepted! Out of range! Guess the number between {start_range} and {official_end_range} (inclusively): "))
             print("--------------------------------------")
 
         # informing user how far off they're WRONG guess is
         if user_guess > correct_num:
-            user_guess = int(input(f"It's lower than that! Guess the number between {start_range} and {end_range} (inclusively): "))
+            user_guess = int(input(f"It's lower than that! Guess the number between {start_range} and {official_end_range} (inclusively): "))
             print("--------------------------------------")
         elif user_guess < correct_num:
-            user_guess = int(input(f"It's higher than that! Guess the number between {start_range} and {end_range} (inclusively): "))
+            user_guess = int(input(f"It's higher than that! Guess the number between {start_range} and {official_end_range} (inclusively): "))
             print("--------------------------------------")
 
     # user guesses correct number
@@ -58,7 +81,3 @@ while not done:
             done = True
             print("See you next time!👋")
             break
-
-    # changes random number after user wins
-    correct_num = random.randint(start_range, end_range) # both numbers i.e. 'start_range' and 'end_range' are included in range!!!
-    print(f"2) correct_num: {correct_num}")
