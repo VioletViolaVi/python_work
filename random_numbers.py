f95f3f7929b405a20efc980f2c4ab3ceefd179b7
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-24 18:24:06 +0000

    practice using radom.randint() with variables

--- random_numbers.py
+++ random_numbers.py
@@ -1,9 +1,18 @@
 # Random Numbers
 
+# Notes:
+    # - random.randint() used to get random whole numbers
+
 import random
 
 # # get help info on random module
 # print(help(random))
+
+
+print()
+print("========= new ========= ")
+print()
+
 
 # gets random whole numbers (between 1 and 12 inclusively)
 card_matching_game_num = random.randint(1, 12) # both start & end numbers are inclusive
@@ -22,3 +31,23 @@ minus_nums = random.randint(-5, -1)
 print(f"minus_nums: {minus_nums}")
 
 
+print()
+print("========= new ========= ")
+print()
+
+
+# random.randint() using variables
+start_num = 1
+end_num = 10
+rand_num_1_to_10 = random.randint(start_num, end_num)
+print(f"rand_num_1_to_10: {rand_num_1_to_10}")
+
+# coin flipping
+heads = 1
+tails = 2
+coin_side = random.randint(heads, tails)
+print(f"coin_side: {coin_side}")
+if coin_side == 1:
+    print("heads!")
+else:
+    print("tails!")
