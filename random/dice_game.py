a55f8165b9cdd169ee261a0ef03417f3753a72fd
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-25 20:58:36 +0000

    create ascii dice art using print statements

--- random/dice_game.py
+++ random/dice_game.py
@@ -1,1 +1,55 @@
 # Dice Game
+
+import random
+
+# 'one' side
+print("+-------+")
+print("|       |")
+print("|   O   |")
+print("|       |")
+print("+-------+")
+
+# 'two' side
+print("+-------+")
+print("| O     |")
+print("|       |")
+print("|     O |")
+print("+-------+")
+
+# 'three' side
+print("+-------+")
+print("| O     |")
+print("|   O   |")
+print("|     O |")
+print("+-------+")
+
+# 'four' side
+print("+-------+")
+print("| O   O |")
+print("|       |")
+print("| O   O |")
+print("+-------+")
+
+# 'five' side
+print("+-------+")
+print("| O   O |")
+print("|   O   |")
+print("| O   O |")
+print("+-------+")
+
+# 'six' side
+print("+-------+")
+print("| O   O |")
+print("| O   O |")
+print("| O   O |")
+print("+-------+")
+
+
+dice_sides = {
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+}
