465cd9eb038d3accd68640d99e849c11b4d498f9
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-25 21:42:23 +0000

    place dice options in while loop to allow users to continuously play

--- random/dice_game.py
+++ random/dice_game.py
@@ -12,62 +12,69 @@ total = 0
 rand_dice_side = random.randint(1, 6)
 print(f"rand_dice_side: {rand_dice_side}")
 
+# helps control number of plays per round
+game_round = 0
+
+# lets user role dice 4 times
+while game_round < 4:
+
 # specific conditions for specific dice sides
-if rand_dice_side == 1:
-    # 'one' side
-    print("+-------+")
-    print("|       |")
-    print("|   O   |")
-    print("|       |")
-    print("+-------+")
+    if rand_dice_side == 1:
+        # 'one' side
+        print("+-------+")
+        print("|       |")
+        print("|   O   |")
+        print("|       |")
+        print("+-------+")
 
-    print(f"You rolled {rand_dice_side}")
-elif rand_dice_side == 2:
-    # 'two' side
-    print("+-------+")
-    print("| O     |")
-    print("|       |")
-    print("|     O |")
-    print("+-------+")
+        print(f"You rolled {rand_dice_side}")
+    elif rand_dice_side == 2:
+        # 'two' side
+        print("+-------+")
+        print("| O     |")
+        print("|       |")
+        print("|     O |")
+        print("+-------+")
 
-    print(f"You rolled {rand_dice_side}")
-elif rand_dice_side == 3:
-    # 'three' side
-    print("+-------+")
-    print("| O     |")
-    print("|   O   |")
-    print("|     O |")
-    print("+-------+")
+        print(f"You rolled {rand_dice_side}")
+    elif rand_dice_side == 3:
+        # 'three' side
+        print("+-------+")
+        print("| O     |")
+        print("|   O   |")
+        print("|     O |")
+        print("+-------+")
 
-    print(f"You rolled {rand_dice_side}")
-elif rand_dice_side == 4:
-    # 'four' side
-    print("+-------+")
-    print("| O   O |")
-    print("|       |")
-    print("| O   O |")
-    print("+-------+")
+        print(f"You rolled {rand_dice_side}")
+    elif rand_dice_side == 4:
+        # 'four' side
+        print("+-------+")
+        print("| O   O |")
+        print("|       |")
+        print("| O   O |")
+        print("+-------+")
 
-    print(f"You rolled {rand_dice_side}")
-elif rand_dice_side == 5:
-    # 'five' side
-    print("+-------+")
-    print("| O   O |")
-    print("|   O   |")
-    print("| O   O |")
-    print("+-------+")
+        print(f"You rolled {rand_dice_side}")
+    elif rand_dice_side == 5:
+        # 'five' side
+        print("+-------+")
+        print("| O   O |")
+        print("|   O   |")
+        print("| O   O |")
+        print("+-------+")
 
-    print(f"You rolled {rand_dice_side}")
-elif rand_dice_side == 6:
-    # 'six' side
-    print("+-------+")
-    print("| O   O |")
-    print("| O   O |")
-    print("| O   O |")
-    print("+-------+")
+        print(f"You rolled {rand_dice_side}")
+    elif rand_dice_side == 6:
+        # 'six' side
+        print("+-------+")
+        print("| O   O |")
+        print("| O   O |")
+        print("| O   O |")
+        print("+-------+")
 
-    print(f"You rolled {rand_dice_side}")
-else:
-    print("Error!")
+        print(f"You rolled {rand_dice_side}")
+    else:
+        print("Error!")
 
-
+    # increases counter so while loop can end
+    game_round += 1
