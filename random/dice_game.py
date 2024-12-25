2984ef6f5fef09d1e3d19be3d09fc32cab4f9605
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-25 21:32:22 +0000

    add print statements to respective conditional statements to ensure correct dice side displays

--- random/dice_game.py
+++ random/dice_game.py
@@ -2,48 +2,6 @@
 
 import random
 
-# 'one' side
-print("+-------+")
-print("|       |")
-print("|   O   |")
-print("|       |")
-print("+-------+")
-
-# 'two' side
-print("+-------+")
-print("| O     |")
-print("|       |")
-print("|     O |")
-print("+-------+")
-
-# 'three' side
-print("+-------+")
-print("| O     |")
-print("|   O   |")
-print("|     O |")
-print("+-------+")
-
-# 'four' side
-print("+-------+")
-print("| O   O |")
-print("|       |")
-print("| O   O |")
-print("+-------+")
-
-# 'five' side
-print("+-------+")
-print("| O   O |")
-print("|   O   |")
-print("| O   O |")
-print("+-------+")
-
-# 'six' side
-print("+-------+")
-print("| O   O |")
-print("| O   O |")
-print("| O   O |")
-print("+-------+")
-
 # counts number of dice used
 dice_quantity = 0
 
@@ -56,16 +14,58 @@ print(f"rand_dice_side: {rand_dice_side}")
 
 # specific conditions for specific dice sides
 if rand_dice_side == 1:
+    # 'one' side
+    print("+-------+")
+    print("|       |")
+    print("|   O   |")
+    print("|       |")
+    print("+-------+")
+
     print(f"You rolled {rand_dice_side}")
 elif rand_dice_side == 2:
+    # 'two' side
+    print("+-------+")
+    print("| O     |")
+    print("|       |")
+    print("|     O |")
+    print("+-------+")
+
     print(f"You rolled {rand_dice_side}")
 elif rand_dice_side == 3:
+    # 'three' side
+    print("+-------+")
+    print("| O     |")
+    print("|   O   |")
+    print("|     O |")
+    print("+-------+")
+
     print(f"You rolled {rand_dice_side}")
 elif rand_dice_side == 4:
+    # 'four' side
+    print("+-------+")
+    print("| O   O |")
+    print("|       |")
+    print("| O   O |")
+    print("+-------+")
+
     print(f"You rolled {rand_dice_side}")
 elif rand_dice_side == 5:
+    # 'five' side
+    print("+-------+")
+    print("| O   O |")
+    print("|   O   |")
+    print("| O   O |")
+    print("+-------+")
+
     print(f"You rolled {rand_dice_side}")
 elif rand_dice_side == 6:
+    # 'six' side
+    print("+-------+")
+    print("| O   O |")
+    print("| O   O |")
+    print("| O   O |")
+    print("+-------+")
+
     print(f"You rolled {rand_dice_side}")
 else:
     print("Error!")
