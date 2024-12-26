c35b2e0a3991c2ec4cc199962030c625e4239d9e
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-26 20:06:25 +0000

    add notes about unpacking arguments with * and **

--- functions/args_kwargs.py
+++ functions/args_kwargs.py
@@ -1,4 +1,9 @@
 # Arguments (*args) and Keyword Arguments (**kwargs)
 
-
+# Notes
+    # - *args and **kwargs are used when you don't know how many arguments will be passed when function is called
+    # - 'args' and 'kwargs' don't have to be used but they are convention among developers
+        # - '*' and '**' are what's important to add in 'def python_func()' brackets
+            # - e.g. *test, **test_dict, *example, **example_dict, etc.
+    # - 
 
