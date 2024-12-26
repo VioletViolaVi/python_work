ebe8690d7b1c5ae55a41caa282c30109373fc37e
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-26 15:09:37 +0000

    practice writing & calling functions

--- functions.py
+++ functions.py
@@ -1,3 +1,22 @@
 # Functions
 
+# happy birthday song
+def happy_birthday_song():
+    print("happy birthday to u")
+    print("u live in a zoo")
+    print("u look like a monkey")
+    print("and u smell like one too!")
+    print()
 
+happy_birthday_song()
+happy_birthday_song()
+
+# say hello world
+def hello_world():
+    print("Hello World! 👋")
+    print()
+
+hello_world()
+hello_world()
+hello_world()
+hello_world()
