7acc7a57ad32d5a854f671ae0ffef3628cf43854
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-27 22:19:11 +0000

    add notes

--- module.py
+++ module.py
@@ -1,3 +1,16 @@
 # Module
 
+# Notes
+    # - putting the name of a module in help() function shows everything contained in the module
+    # - can use alias when importing modules
+    #    - 'import math' becomes 'import math as m', 'm' would be used instead of 'math' e.g. 'm.random()', 'm.pi()'
+    #    - alias could be useful if module name is really long
+    # - 
+
+# # shows full list of modules
+# print(help("modules"))
+
+# # shows all features present in math module
+# print(help("math"))
+
 
