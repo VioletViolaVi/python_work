07d38c565a40d0bea17ff9884ec80a6db010d727
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-27 17:16:08 +0000

    practice using 'not in' for letter guessing game

--- membership_operators.py
+++ membership_operators.py
@@ -1,3 +1,16 @@
 # Membership Operators
 
+# word guessing game
 
+# word user needs to guess right
+answer = "apple"
+
+# greeting for user
+print("Can you guess the word?")
+# stores user's guess
+user_letter_guess = input("Guess a letter in the word: ").lower()
+
+if user_letter_guess not in answer or user_letter_guess == "":
+    print(f"No. '{user_letter_guess}' is not in the word!")
+else:
+    print(f"Yes. '{user_letter_guess}' is in the word!")
