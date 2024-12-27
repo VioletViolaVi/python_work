68643f5806053301975006cde7cf3262c4d4feaf
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-27 21:21:17 +0000

    practice writing match case statements

--- match_case_statement.py
+++ match_case_statement.py
@@ -1,7 +1,7 @@
 # Match Case Statements
 
 # Notes
-    # - 
+    # - match case statements are alternatives to using lots of elif statements
     # - 
 
 
@@ -25,5 +25,56 @@ def find_day(day):
         print(f"It's {day}!")
     else:
         print("Error!")
-
 find_day(day)
+
+
+print()
+
+
+# finding days of week using match case statements
+def find_day(day):
+    match day:
+        case "Monday":
+            print(f"It's {day}!")
+        case "Tuesday":
+            print(f"It's {day}!")
+        case "Wednesday":
+            print(f"It's {day}!")
+        case "Thursday":
+            print(f"It's {day}!")
+        case "Friday":
+            print(f"It's {day}!")
+        case "Saturday":
+            print(f"It's {day}!")
+        case "Sunday":
+            print(f"It's {day}!")
+        case _: # this ( _ ) is a wildcard & this case occurs if there's no matching cases from above
+            print("Error!")
+find_day(day)
+
+
+print(" --- next --- ")
+
+
+# finds out which are weekends
+user_input = input("Is it a weekend? Enter a day: ").capitalize()
+
+def is_weekend(user_input):
+    match user_input:
+        case "Monday":
+            print("No. This is a weekday.")
+        case "Tuesday":
+            print("No. This is a weekday.")
+        case "Wednesday":
+            print("No. This is a weekday.")
+        case "Thursday":
+            print("No. This is a weekday.")
+        case "Friday":
+            print("No. This is a weekday.")
+        case "Saturday":
+            print("Yes. This is a weekend.")
+        case "Sunday":
+            print("Yes. This is a weekend.")
+        case _:
+            print("Error!")
+is_weekend(user_input)
