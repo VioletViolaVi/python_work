c66b52c059cd79b0c127b77ca7b2708ae3c12864
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-27 18:50:48 +0000

    write for loop in full

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,3 +1,15 @@
 # List Comprehension
 
+# Notes:
+    # - 
+    # - 
+    # -
 
+# iterating to double numbers from 1-10 inclusively
+original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+doubled = []
+for num in range(1,11):
+    num *= 2
+    doubled.append(num)
+print(original)
+print(doubled)
