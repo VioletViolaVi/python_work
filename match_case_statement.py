e78e0e1fe28a1a128f513ae7e7bd4dc6528c1d20
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-27 20:58:11 +0000

    create if/elif/else statements to find the day entered by user

--- match_case_statement.py
+++ match_case_statement.py
@@ -1,3 +1,29 @@
-# Match Case Statement
+# Match Case Statements
+
+# Notes
+    # - 
+    # - 
 
 
+# finding days of week using if/elif/else
+day = input("What day is it today?: ").capitalize()
+
+def find_day(day):
+    if day == "Monday":
+        print(f"It's {day}!")
+    elif day == "Tuesday":
+        print(f"It's {day}!")
+    elif day == "Wednesday":
+        print(f"It's {day}!")
+    elif day == "Thursday":
+        print(f"It's {day}!")
+    elif day == "Friday":
+        print(f"It's {day}!")
+    elif day == "Saturday":
+        print(f"It's {day}!")
+    elif day == "Sunday":
+        print(f"It's {day}!")
+    else:
+        print("Error!")
+
+find_day(day)
