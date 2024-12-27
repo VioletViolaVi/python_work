312c3c0423ba1a7d14c5ce528a26be19c389b407
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-27 16:42:57 +0000

    practice working with iterables regarding lists

--- iterables.py
+++ iterables.py
@@ -1,2 +1,15 @@
 # Iterables
 
+# list of colours
+colours = ["red", "green", "yellow", "orange", "purple"]
+
+# give single items being printed out readable names
+for colour in colours:
+    print(f"colour: {colour}")
+
+print()
+
+# prints list backwards
+for colour in reversed(colours):
+    print(f"colour: {colour}", end=" ][ ")
+print()
