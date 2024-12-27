3e44d753f8e22f6e0c57a95d5010857c078636de
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-27 22:42:24 +0000

    add notes to custom made module

--- modules/own_module.py
+++ modules/own_module.py
@@ -4,15 +4,18 @@
 
 # adds 2 numbers together
 def add_num(num_1, num_2):
-    return num_1 + num_2
+    # using int() can help ensure a number is returned even if user entered a string
+    return int(num_1) + int(num_2) 
 
 # squares number passed through
-def square_num(num):
-    return num ** 2
+def squaring_num(num):
+    # using int() can help ensure a number is returned even if user entered a string
+    return int(num) ** 2
 
 # finds length of string
-def count_str_char(str):
-    return len(str)
+def count_str_chars(string):
+    # using str() can help ensure a string is returned even if user entered a number
+    return len(str(string))
 
 # converts data into list
 def convert_to_list(item):
