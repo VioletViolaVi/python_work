8ceab6ab73181d2b09fc231c75db7302ceaec4eb
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-27 22:44:09 +0000

    import custom made module & called its file's functions to complete tasks

--- modules/module.py
+++ modules/module.py
@@ -1,4 +1,4 @@
-# Module
+# Modules
 
 # Notes
     # - putting the name of a module in help() function shows everything contained in the module
@@ -7,11 +7,11 @@
     #    - alias could be useful if module name is really long
     # - 
 
-# # shows full list of modules
-# print(help("modules"))
+# shows full list of modules
+print(help("modules"))
 
-# # shows all features present in math module
-# print(help("math"))
+# shows all features present in math module
+print(help("math"))
 
 # different ways to import modules
 import math
@@ -27,3 +27,18 @@ from math import pi, ceil
 # didn't need to use dot notation
 print(pi)
 print(ceil(9.1))
+
+# imported module from custom made 'own_module.py' file
+import own_module
+
+# adds 2 numbers passed through as arguments
+print(own_module.add_num(10, "12")) # prints 22, as they were added together
+
+# times number passed through to the power of 2
+print(own_module.squaring_num("5")) # prints 25
+
+# counts number of characters in string
+print(own_module.count_str_chars("this hand is my hand!"))
+
+# changes data into a list
+print(own_module.convert_to_list("long live the death"))
