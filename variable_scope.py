d373263373c135c7c2d03ab2173bf341a13e5580
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-28 17:47:38 +0000

    create functions with local scope

--- variable_scope.py
+++ variable_scope.py
@@ -1,3 +1,13 @@
 # Variable Scope
 
+# functions w/ local scope
+def function_1():
+    func_1 = 10
+    print(func_1)
 
+def function_2():
+    func_2 = 5
+    print(func_2)
+
+function_1()
+function_2()
