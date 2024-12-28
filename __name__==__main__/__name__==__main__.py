6ed830510a1aa3e103d4581c2e429430a030bef1
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-28 21:33:26 +0000

    add additional notes

--- __name__==__main__/__name__==__main__.py
+++ __name__==__main__/__name__==__main__.py
@@ -1,4 +1,22 @@
 # __name__ == "__main__"
 
+# Notes
+# __name__ == "__main__" means the .py it's in can be imported into other files or it can run on its own
+    # - functions & methods in .py files that contain the __name__ == "__main__" can be used w/ out running the main block of code in the file when the conditional statement is used i.e. if __name__ == "__main__":
+
+# examples when you'd want to import functions/methods from .py files w/ out their main function body running:
+    # - importing modules like math, random, time, os, etc.
+        # - if not, outputs like what the help() produces will be created i.e. the full help page
+            # - this way help pages can be made to run only if they are being ran directly
+
+# __name__ == "__main__" 
+    # - helps avoid unintentional execution of code
+    # - makes code more modular
+    # - makes code more readable
+    # - leaves no global variables 
+
 # provided list of attributes available
 print(dir())
+
+# prints out __main__
+print(__name__)
--- __name__==__main__/script_1.py
+++ __name__==__main__/script_1.py
@@ -16,5 +16,5 @@ def simple_func():
 
 # - controls if script_2.py will be able to run this function from this file or if it can only run the called function from the called function written in its own file
 #   - code below will not allow script_2.py from running the function from this file
-if __name__ == "__main__":
+if __name__ == "__main__": # this is asking if you are running the file directly, if yes = truthy, if no = falsy
     simple_func()  
