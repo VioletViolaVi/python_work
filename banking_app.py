4334fa94074aac037ae34659ed3ebe1f9d93c524
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-28 22:21:07 +0000

    create code to give user option to deposit money

--- banking_app.py
+++ banking_app.py
@@ -1,20 +1,48 @@
 # Banking App
 
 # user is greeted to their bank account
-print("Welcome to your bank account.How can we help you?")
+print("Welcome to your bank account. How can we help you?")
+print(" ------ ")
 
 # user picks which action they want to take
-user_request =  input("""Enter 1, 2, 3 or 4
-1) Deposit money
-2) Withdraw money
-3) Check balance
-4) Exit
-""")
+user_request =  input("Enter number 1) Deposit money, 2) Withdraw money, 3) Check balance, 4) Exit: ") # returns strings!
+print(" ------ ")
 
 # helps user leave banking process
 is_banking = True
 
+# starts current balance amount as 100 (£)
+current_balance = 100
+
 # keeps user w/ in process as they have not quit
 while is_banking:
-    pass
+
+    # keeps asking user to enter valid option until they enter a valid option
+    # if users pick from outside of provided options
+    while not user_request == "1" and not user_request == "2" and not user_request == "3" and not user_request == "4": # uses strings not numbers!
+        user_request = input("Invalid option! Enter number 1) Deposit money, 2) Withdraw money, 3) Check balance, 4) Exit: ")
+        print(" ------ ")
+
+    # displays current bank balance
+    print(f"Your current bank balance is £{current_balance:.2f}")
+    print(" ------ ")
+        
+    # user wants to deposit money into bank - keeps going until they choose a different option
+    while user_request == "1": # uses string not number!
+
+        # converts string inputted amount into float number
+        deposit_amount =  float(input("Please enter amount you want to deposit: £ "))
+
+        # stores the addition of deposit amount to current amount in account
+        new_balance = current_balance + deposit_amount
+
+        # displays new balance after deposit
+        print(f"Your new bank balance is £{new_balance:.2f}")
+        print(" ------ ")
+
+        # asks user what they want to do next
+        user_request = input("What would you like to do next? Enter number 1) Deposit money, 2) Withdraw money, 3) Check balance, 4) Exit: ")
+        print(" ------ ")
+
+
 
