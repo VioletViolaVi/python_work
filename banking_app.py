8b9126e18dd0cae4bc14fadfec83f82ebcd10139
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-28 21:56:25 +0000

    add starter code for user to enter desired banking option

--- banking_app.py
+++ banking_app.py
@@ -1,2 +1,20 @@
 # Banking App
 
+# user is greeted to their bank account
+print("Welcome to your bank account.How can we help you?")
+
+# user picks which action they want to take
+user_request =  input("""Enter 1, 2, 3 or 4
+1) Deposit money
+2) Withdraw money
+3) Check balance
+4) Exit
+""")
+
+# helps user leave banking process
+is_banking = True
+
+# keeps user w/ in process as they have not quit
+while is_banking:
+    pass
+
