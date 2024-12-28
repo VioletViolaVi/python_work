8e56308b46dbd39b0db7df9054f635c6a2be9ec9
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-28 20:40:25 +0000

    practice running script_2.py file, with imports from script_1.py, to produce same output twice

--- __name__==__main__/script_1.py
+++ __name__==__main__/script_1.py
@@ -1,4 +1,16 @@
-print(f"script_1.py __name__ == {__name__}")
+# script_1.py
 
 # Notes
 #   - from this file, when ran, output produces -> script_1.py __name__ == __main__
+
+
+# shows in output when code is ran from this file
+# __name__ becomes __main__
+print(f"script_1.py __name__ == {__name__}")
+
+print()
+
+# simple function made, for printing 'hello world' so script_2.py can be checked to see if the file runs it
+def simple_func():
+    print("Hello World!")
+simple_func()
--- __name__==__main__/script_2.py
+++ __name__==__main__/script_2.py
@@ -1,5 +1,13 @@
-import script_1
+# script_2.py
 
 # Notes
 #   - from this file, when ran, output produces -> script_1.py __name__ == script_1
 #       - script_1 is written instead of __name__ as code is being ran from this file not script_1.py
+
+
+import script_1 # prints out hello world in output when this part is ran
+
+# uses function from script_1.py file that was imported to this file
+script_1.simple_func() # also prints out hello world in output when this part is ran
+
+# - at this point, output shows hello world twice
