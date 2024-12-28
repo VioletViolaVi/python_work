29d6d489270f45197ebea1569276a3b1c2c4a81b
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-28 20:33:32 +0000

    add notes

---
+++ __name__==__main__/script_1.py
@@ -0,0 +1,4 @@
+print(f"script_1.py __name__ == {__name__}")
+
+# Notes
+#   - from this file, when ran, output produces -> script_1.py __name__ == __main__
---
+++ __name__==__main__/script_2.py
@@ -0,0 +1,5 @@
+import script_1
+
+# Notes
+#   - from this file, when ran, output produces -> script_1.py __name__ == script_1
+#       - script_1 is written instead of __name__ as code is being ran from this file not script_1.py
