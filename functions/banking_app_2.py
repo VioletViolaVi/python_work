d7a7426217785df2ae146c4790e58db75fb932ba
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-29 19:20:06 +0000

    remove functions from main() & place mach case statements in main()

--- functions/banking_app_2.py
+++ functions/banking_app_2.py
@@ -10,29 +10,44 @@ user_balance = 100
 # functions ----------------------------------------------------------------------------------------------------------------------------
 
 # helps prevent code running when this file is ran from different file's server
-def main():
-    # checks balance
-    def check_balance():
-        message = f"Your current balance is {user_balance:.2f}"
-        return message
 
-    # deposits in balance (adds to balance)
-    def to_deposit():
-        deposit_amount = float(input("How much do you want to deposit into your account?: £ "))
-        user_balance += deposit_amount
-        return user_balance
+# checks balance
+def check_balance():
+    message = f"Your current balance is {user_balance:.2f}"
+    return message
 
-    # withdraws from balance
-    def withdraws_from_balance():
-        withdrawal_amount = float(input("How much do you want to withdraw from your account?: £ "))
-        user_balance -= withdrawal_amount
-        return user_balance
+# deposits in balance (adds to balance)
+def to_deposit():
+    deposit_amount = float(input("How much do you want to deposit into your account?: £ "))
+    user_balance += deposit_amount
+    return user_balance
 
-    # leaves banking app
-    def leaves_bank_app():
-        print("Thank you for banking with us. Goodbye.")
+# withdraws from balance
+def withdraws_from_balance():
+    withdrawal_amount = float(input("How much do you want to withdraw from your account?: £ "))
+    user_balance -= withdrawal_amount
+    return user_balance
+
+# leaves banking app
+def leaves_bank_app():
+    print("Thank you for banking with us. Goodbye.")
+
 
 # functions ----------------------------------------------------------------------------------------------------------------------------
+
+def main():
+    # controls what users see based on option they choose
+    match user_input:
+        case "1":
+            check_balance()
+        case "2":
+            to_deposit()
+        case "3":
+            withdraws_from_balance()
+        case "4":
+            leaves_bank_app()
+        case _:
+            print("Error!")
 
 # so functions in this file don't have to be run from this file's server
 if __name__ == "__main__":
