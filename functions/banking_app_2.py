8c9e073371c3555535996ba68a02032955b8074e
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-29 19:01:02 +0000

    add functions needed

--- functions/banking_app_2.py
+++ functions/banking_app_2.py
@@ -1,3 +1,27 @@
 # Banking App (function focussed)
 
 
+# functions ----------------------------------------------------------------------------------------------------------------------------
+
+# checks balance
+def check_balance():
+    pass
+
+# deposits in balance (adds to balance)
+def deposit_in_balance():
+    pass
+
+# withdraws from balance
+def withdraws_from_balance():
+    pass
+
+# leaves banking app
+def leaves_bank_app():
+    pass
+
+# helps prevent code running when this file is ran from different file's server
+def main():
+    pass
+
+# functions ----------------------------------------------------------------------------------------------------------------------------
+
