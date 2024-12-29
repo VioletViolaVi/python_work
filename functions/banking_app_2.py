d51f3c8bf3f39a6826e3785f4a2d8f982540ec65
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-29 19:09:09 +0000

    move functions into main()

--- functions/banking_app_2.py
+++ functions/banking_app_2.py
@@ -9,30 +9,28 @@ user_balance = 100
 
 # functions ----------------------------------------------------------------------------------------------------------------------------
 
-# checks balance
-def check_balance():
-    message = f"Your current balance is {user_balance:.2f}"
-    return message
-
-# deposits in balance (adds to balance)
-def to_deposit():
-    deposit_amount = float(input("How much do you want to deposit into your account?: £ "))
-    user_balance += deposit_amount
-    return user_balance
-
-# withdraws from balance
-def withdraws_from_balance():
-    withdrawal_amount = float(input("How much do you want to withdraw from your account?: £ "))
-    user_balance -= withdrawal_amount
-    return user_balance
-
-# leaves banking app
-def leaves_bank_app():
-    print("Thank you for banking with us. Goodbye.")
-
 # helps prevent code running when this file is ran from different file's server
 def main():
-    pass
+    # checks balance
+    def check_balance():
+        message = f"Your current balance is {user_balance:.2f}"
+        return message
+
+    # deposits in balance (adds to balance)
+    def to_deposit():
+        deposit_amount = float(input("How much do you want to deposit into your account?: £ "))
+        user_balance += deposit_amount
+        return user_balance
+
+    # withdraws from balance
+    def withdraws_from_balance():
+        withdrawal_amount = float(input("How much do you want to withdraw from your account?: £ "))
+        user_balance -= withdrawal_amount
+        return user_balance
+
+    # leaves banking app
+    def leaves_bank_app():
+        print("Thank you for banking with us. Goodbye.")
 
 # functions ----------------------------------------------------------------------------------------------------------------------------
 
