0307710d1e6fb942283b4d09caa3425b0ade0d5a
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-30 15:01:06 +0000

    create main() & conditional statement so file can be imported into other files without running automatically

--- functions/slot_machine.py
+++ functions/slot_machine.py
@@ -1,3 +1,9 @@
 # Slot Machine
 
+# helps stops this file running automatically if this file is used in another file via imports 
+def main():
+    pass
 
+# stops this file running automatically if this file is used in another file via imports
+if __name__ == "__main__":
+    main()
