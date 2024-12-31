4609049e276ed59aa21eb41e3bc82df0571c6e9f
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-31 21:06:24 +0000

    create list & copy using all printable characters

--- sentence_encryption.py
+++ sentence_encryption.py
@@ -2,6 +2,15 @@
 
 import string
 
+# stores all different characters available
 chars = string.printable
 
+# converts chars "string" into [list]
+chars_list = list(chars)
+
+# creates copy of chars [list]
+chars_list_copy = chars_list.copy()
+
 print(chars)
+print(chars_list)
+print(chars_list_copy)
