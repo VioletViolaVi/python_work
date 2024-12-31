8df7e4181df634c13a277a8a77d209b758b9ecc3
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-12-31 22:03:51 +0000

    use for loop for changing characters entered by users, to encrypt message

--- sentence_encryption.py
+++ sentence_encryption.py
@@ -17,10 +17,22 @@ random.shuffle(chars_list_copy)
 # stores shuffled characters
 shuffled_chars_list_copy = chars_list_copy
 
-print(f"chars: {chars}")
-print()
-print(f"chars_list: {chars_list}")
-print()
-print(f"chars_list_copy: {chars_list_copy}")
-print()
-print(f"shuffled chars_list_copy: {shuffled_chars_list_copy}")
+# get sentence from user to encrypt
+user_sentence = input("Enter a sentence to encrypt: ")
+
+# stores encrypted message
+encrypted_sentence = ""
+
+# replace each user sentence character with character from [shuffled_chars_list_copy list]
+for char in user_sentence:
+
+    # gets random number for selecting random char from shuffled list
+    # uses length of [shuffled_chars_list_copy list] for passed number value
+    rand_num = random.randint( 0, ( len(shuffled_chars_list_copy) - 1 ) )
+
+    # stores random character from [shuffled_chars_list_copy list] for every character entered in user sentence
+    encrypted_sentence += shuffled_chars_list_copy[rand_num]
+    
+    print(f"char: {char} | encrypted_sentence: {encrypted_sentence}")
+
+print(f"The final encrypted message: {encrypted_sentence}")
