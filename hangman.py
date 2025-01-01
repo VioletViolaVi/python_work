7e1186aae163cdc8f2b8712fb5ced74839fd2c08
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-01 17:53:18 +0000

    add basic structure to hangman game

--- hangman.py
+++ hangman.py
@@ -1,3 +1,31 @@
 # Hangman
 
+# todo - basics:
+# - show dashes
+# - show correct letters
+# - show parts of hangman when wrong letters
+# - game over & correct word
+# todo - additions:
+# - scores
+# - continuous play
+# - greeting & goodbye
 
+def show_dashes():
+    pass
+
+def show_correct_letters():
+    pass
+
+def show_hangman():
+    pass
+
+def show_full_answer():
+    pass
+
+# helps allow file to run from other files w/out repeats
+def main():
+    pass
+
+# allows file to run from other files w/out repeats
+if __name__ == "__main__":
+    main()
