9d6021e7277a873bf5d6b7c80147981626ffc0f5
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-02 19:36:30 +0000

    create variables for game

--- functions/hangman_2.py
+++ functions/hangman_2.py
@@ -39,20 +39,39 @@ hangman_art = {
 # functions
 
 # shows correct hangman art based on num of wrong guesses dict key
-def display_man(wrong_guesses):
+def display_hangman_art(incorrect_guess_num):
     pass
 
 # shows [list] of dashes for letters to guess
-def display_hint(hint):
+def display_dashes(dashes):
     pass
 
 # shows correct answer when game is won or lost
-def display_answer(answer):
+def display_answer(correct_word):
     pass
 
 # to contain main body of code
 def app():
-    pass
+    
+    # gets random word from [words] list above
+    correct_word =  random.choice(words)
+    
+    # produces same num of dashes as there are in word to guess
+    dashes = " _ " * len(correct_word)
+
+    # stores num of incorrect guesses made so correct hangman art can be shown
+    incorrect_guess_num = 0
+
+    # stores all letters guessed by user, wrong or not
+    all_user_guesses = ""
+
+    # to turn game on and off once game has been won/lost
+    is_playing = True
+
+
+
+    print(f"correct_word: {correct_word}")
+    print(f"dashes: {dashes}")
 
 # app() function gets called only when this code is ran directly
 if __name__ == "__main__":
