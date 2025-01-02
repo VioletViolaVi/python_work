4717906c8f6de471a1df1ca9dad9c666ef637ba8
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-02 19:05:42 +0000

    add art for hangman

--- functions/hangman_2.py
+++ functions/hangman_2.py
@@ -1,3 +1,43 @@
 # Hangman Game
 
 import random
+
+# stores words for game
+words = ["lunch", "knee", "it", "fly", "lollipop"]
+
+# different hangman states - {num of wrong guesses: art}
+hangman_art = {
+    0 : ("   ", 
+         "   ", 
+         "   "),
+
+    1 : (" O ", 
+         "   ", 
+         "   "),
+
+    2 : (" O ", 
+         " | ", 
+         "   "),
+
+    3 : (" O ", 
+         "/| ", 
+         "   "),
+
+    4 : (" O ", 
+         "/|\\", 
+         "   "),
+
+    5 : (" O ", 
+         "/|\\", 
+         "/  "),
+
+    6 : (" O ", 
+         "/|\\", 
+         "/ \\")
+}
+
+x = 0
+while x <= 6:
+    for line in hangman_art[x]:
+        print(line)
+    x += 1
