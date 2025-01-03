53c52196304314f0ff6fa74dad47c1db3b1eb492
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-03 22:15:56 +0000

    add notes about oop

--- oop/object_oriented_programming.py
+++ oop/object_oriented_programming.py
@@ -1,1 +1,21 @@
 # Object Oriented Programming
+
+# Notes:
+
+# objects:
+# - these are a bundle of related attributes & methods
+#   - e.g. car, person, book, mobile, etc. (any 'noun' thing really)
+
+# attributes:
+# - these are similar to variables
+# - they describe what object has
+
+# methods:
+# - they are functions inside the object
+# - these functions belong to the object
+
+# classes:
+# - they are used to make many objects
+# - they are a type of blueprint used to design layout & structure of an object
+# - they are used to design the attributes & methods of an object
+#   - what do the objects have (attributes) and what do the objects do (methods)
