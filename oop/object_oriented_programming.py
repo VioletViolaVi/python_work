f31b459a2e02e11703bb862fc162850c920855da
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-03 22:39:07 +0000

    create Car class

--- oop/object_oriented_programming.py
+++ oop/object_oriented_programming.py
@@ -5,6 +5,7 @@
 # objects:
 # - these are a bundle of related attributes & methods
 #   - e.g. car, person, book, mobile, etc. (any 'noun' thing really)
+# - 'constructors' are required to construct objects
 
 # attributes:
 # - these are similar to variables
@@ -19,3 +20,18 @@
 # - they are a type of blueprint used to design layout & structure of an object
 # - they are used to design the attributes & methods of an object
 #   - what do the objects have (attributes) and what do the objects do (methods)
+
+
+# creating objects using classes
+# class of Car
+class Car:
+
+    # this is a constructor, it is need to create objects
+    # in brackets, parameters are being passed through, they'll be replaced w/ real values passed through when object is being created
+    def __init__(self, model, year, colour, for_sale):
+
+        # assigning passed through values to objects (the part starting with 'self.')
+        self.model = model
+        self.year = year
+        self.colour = colour
+        self.for_sale = for_sale
