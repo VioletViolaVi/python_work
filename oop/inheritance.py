0b759fc509e989b6d5e3c01461ab1f2874833417
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-04 22:26:57 +0000

    create parent class with constructor, attributes & methods

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -1,3 +1,22 @@
 # Inheritance
 
+# Notes:
+
+# 
+
+# parent class
+class Animal:
+
+    # constructor
+    def __init__(self, name, is_alive):
+        self.name = name
+        self.is_alive = is_alive
+
+    # methods
+    def eating(self):
+        return f"The {self.name} is eating."
+
+    def sleeping(self):
+        return f"The {self.name} is sleeping."
+
 
