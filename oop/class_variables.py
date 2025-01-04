d51e2d4bb6663fee2e36a3cf3a23d14c15caac16
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-04 19:00:10 +0000

    add notes

--- oop/class_variables.py
+++ oop/class_variables.py
@@ -1,3 +1,15 @@
 # Class Variables
 
+# Notes:
+
+# - class variables are variables shared among all instances/objects made from the same class
+
+# instance variables:
+    # - these are defined INSIDE a constructor
+    # - each object has their own of these variables
+
+# class variables:
+    # - these are defined OUTSIDE a constructor
+    # - these can share their data with all objects made from the same class
+
 
