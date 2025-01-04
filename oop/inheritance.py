714e3f9973f68dff21fd22d24ce3cafeb6db50e8
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-04 22:27:47 +0000

    create objects from parent class

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -19,4 +19,14 @@ class Animal:
     def sleeping(self):
         return f"The {self.name} is sleeping."
 
+# creating an object then assigning the object to a variable
+animal_obj_1 = Animal("Daffy", True)
+animal_obj_2 = Animal("Bugs", False)
+print()
+print(f"Animal 1's name is: {animal_obj_1.name}")
+print(f"Is animal 1 dead or alive?: {'Alive! 😊' if animal_obj_1.is_alive else 'Dead! 😢'}")
+print()
+print(f"Animal 2's name is: {animal_obj_2.name}")
+print(f"Is animal 2 dead or alive?: {'Alive! 😊' if animal_obj_2.is_alive else 'Dead! 😢'}")
+
 
