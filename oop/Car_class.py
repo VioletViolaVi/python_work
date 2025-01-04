0d1d3448c8d88adc75e1076a2c36f86edb83d35a
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-04 16:19:20 +0000

    add Car class to file

--- oop/Car_class.py
+++ oop/Car_class.py
@@ -1,2 +1,16 @@
 # Car class - placed in own file
 
+
+# creating objects using classes
+# class of Car
+class Car:
+
+    # this is a constructor, it is need to create objects
+    # in brackets, parameters are being passed through, they'll be replaced w/ real values passed through when object is being created
+    def __init__(self, model, year, colour, for_sale):
+
+        # assigning passed through values to objects (the part starting with 'self.')
+        self.model = model
+        self.year = year
+        self.colour = colour
+        self.for_sale = for_sale
