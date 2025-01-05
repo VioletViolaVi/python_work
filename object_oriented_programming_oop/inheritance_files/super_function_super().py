b77657c82d2c093a158530170e1966b319b2141f
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-05 21:33:47 +0000

    add notes

--- object_oriented_programming_oop/inheritance_files/super_function_super().py
+++ object_oriented_programming_oop/inheritance_files/super_function_super().py
@@ -1,3 +1,14 @@
 # Super Function - super()
 
+# Notes:
+# super()
+    # - it's a function
+    # - used in child classes
+    # - calls attributes/methods from parent class into child classes
+    # - extends functionality of inherited methods
+        # - e.g. putting 'super()' in a (grand) child class's method
+
+# child class == subclass
+# parent class == superclass
+
 
