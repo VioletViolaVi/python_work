26dd9dc5f26f3803948bb824e7385f9192cd7331
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-05 18:42:03 +0000

    create parent & children classes

--- oop/inheritance_files/multiple_and_multilevel_inheritance.py
+++ oop/inheritance_files/multiple_and_multilevel_inheritance.py
@@ -11,3 +11,31 @@
         # - child class of parent class, inheriting from the other parent class, will also inherit methods/attributes from the (grand) parent class
             # - child class: C, child's parent class: B, parent class of the child's parent class: A
                 # - C inherits from B, C also inherits from A because B inherits from A & C inherits everything from B
+
+
+# parent class -------------------------------------------------
+
+# prey class
+class Prey():
+    pass
+
+# predator class
+class Predator():
+    pass
+
+
+# child class --------------------------------------------------
+
+# killer whales class - sharks eat penguins
+class Killer_Whales():
+    pass
+
+# penguins class - penguins eat fish, penguins get eaten by sharks
+class Penguins():
+    pass
+
+
+# fish class - fish eat other fish, fish gets eaten by penguins & sharks
+class Fish():
+    pass
+
