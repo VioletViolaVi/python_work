596b3c3b85d8503f1a4b54d564c51e12a4be3ffe
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-05 18:29:52 +0000

    add notes

--- oop/inheritance_files/multiple_and_multilevel_inheritance.py
+++ oop/inheritance_files/multiple_and_multilevel_inheritance.py
@@ -1,3 +1,13 @@
 # Multiple Inheritance & Multilevel Inheritance
 
+# Notes:
+# multiple inheritance
+    # - occurs when child class inherits from more than 1 parent class
+    # - syntax structure: 
+        # - class name_of_child_class(name_of_parent_class, name_of_parent_class_2, name_of_parent_class_3):
 
+# multilevel inheritance
+    # - occurs when parent class inherits from another parent class
+        # - child class of parent class, inheriting from the other parent class, will also inherit methods/attributes from the (grand) parent class
+            # - child class: C, child's parent class: B, parent class of the child's parent class: A
+                # - C inherits from B, C also inherits from A because B inherits from A & C inherits everything from B
