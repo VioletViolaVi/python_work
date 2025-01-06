8b4ebecf9577ca3e1b115be490fe67bcf099d4e9
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-06 20:10:35 +0000

    create classes

--- object_oriented_programming_oop/inheritancing_classes/polymorphism/duck_typing_polymorphism.py
+++ object_oriented_programming_oop/inheritancing_classes/polymorphism/duck_typing_polymorphism.py
@@ -10,6 +10,22 @@
         # - examples of equal treatment:
             # 1) can use the unrelated class w/ the actual children classes in a for loop iterating over a [list] w/out an error
             # 2) a class can use methods and/or attributes from a class that's NOT its parent, as long as this class has same methods and/or attributes
-# - 
 
 
+# create classes
+
+# parent class
+class Rooms():
+    pass
+
+# child class
+class Bathroom(Rooms):
+    pass
+
+# child class
+class Bedroom(Rooms):
+    pass
+
+# NOT ONE OF THE CHILD CLASSES!!!
+class Limo():
+    pass
