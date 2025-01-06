f9df5d220058e11eba6cb6c09131b1ca2439bc52
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-06 19:05:46 +0000

    create Pancake class & pancake object, not related to parent class, to demonstrate error due no inherited parent class access

--- object_oriented_programming_oop/inheritancing_classes/polymorphism.py
+++ object_oriented_programming_oop/inheritancing_classes/polymorphism.py
@@ -45,25 +45,32 @@ class Pentagon(Shape):
     
     # is overriding 'calculate_perimeter()' method from its parent being inherited from
     def calculate_perimeter(self):
-        return f"The perimeter of {self.name} is {self.perimeter}cm at {self.length}cm."
+        return f"The perimeter of {self.name} is {self.perimeter}cm at {self.length}cm per side."
 
 class Hexagon(Shape):
     
     # is overriding 'calculate_perimeter()' method from its parent being inherited from
     def calculate_perimeter(self):
-        return f"The perimeter of {self.name} is {self.perimeter}cm at {self.length}cm."
+        return f"The perimeter of {self.name} is {self.perimeter}cm at {self.length}cm per side."
 
 class Nonagon(Shape):
     
     # is overriding 'calculate_perimeter()' method from its parent being inherited from
     def calculate_perimeter(self):
-        return f"The perimeter of {self.name} is {self.perimeter}cm at {self.length}cm."
+        return f"The perimeter of {self.name} is {self.perimeter}cm at {self.length}cm per side."
 
 class Decagon(Shape):
     
     # is overriding 'calculate_perimeter()' method from its parent being inherited from
     def calculate_perimeter(self):
-        return f"The perimeter of {self.name} is {self.perimeter}cm at {self.length}cm."
+        return f"The perimeter of {self.name} is {self.perimeter}cm at {self.length}cm per side."
+
+
+# unrelated class to 'Shape()'
+class Pancake():
+    # constructor
+    def __init__(self, flavour):
+        self.flavour = flavour
 
 
 # objects made from classes
@@ -89,12 +96,19 @@ nonagon = Nonagon(name="nonagon", sides=9, length=5)
     # this is NOT a Pentagon(), Hexagon() or Nonagon()
 decagon = Decagon(name="decagon", sides=10, length=5)
 
+# creates pancake object from 'Pancake()' class
+pancake = Pancake(flavour="plain")
+
+
 
 # list of objects instantiated from child classes, inheriting from parent class
 
 # same as below:
     # - shape_list = [Pentagon(), Hexagon(), Nonagon(), Decagon()]
-shape_list = [pentagon, hexagon, nonagon, decagon]
+# if an object, that is not inheriting from the parent class, is added to this list an error will occur 
+    # - error would be as a result of not being able to access the methods/attributes from the parent class as no inheritance is occurring 
+        # - 'AttributeError: '[name of class]' object has no attribute '[name of method]''
+shape_list = [pentagon, hexagon, nonagon, decagon, pancake]
 
 # looping through objects list to use parent class method on each object - as they're all inheriting the method from the parent class
 # these child classes where able to use the 'how_many_sides()' method from the parent class due to polymorphism
