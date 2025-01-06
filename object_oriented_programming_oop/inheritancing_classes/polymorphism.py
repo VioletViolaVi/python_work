725fb57168fe1cc899cd88cf8f75e5de584d0908
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-06 16:27:40 +0000

    add notes

--- object_oriented_programming_oop/inheritancing_classes/polymorphism.py
+++ object_oriented_programming_oop/inheritancing_classes/polymorphism.py
@@ -1,3 +1,15 @@
 # Polymorphism
 
+# Notes
+
+# polymorphism 
+    # - means to have many forms/faces
+    # - poly: many
+    # - morphe: form
+
+# objects can take 1 of many forms
+
+# 2 ways to make polymorphism happen 
+    # - inheritance 
+    # - duck typing
 
