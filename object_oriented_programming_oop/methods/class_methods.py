42829f9037927e15a98cba24b0ab921ac8614e92
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-07 17:20:26 +0000

    add notes

--- object_oriented_programming_oop/methods/class_methods.py
+++ object_oriented_programming_oop/methods/class_methods.py
@@ -1,4 +1,9 @@
 # Class Methods
 
+# Notes:
 
+# class methods use 'cls' as their keyword parameter in their brackets
+    # - e.g. def class_method(cls)...
+        # - similar to how instance methods use 'self'
+# - 
 
