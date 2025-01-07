a745401005fc266e9dfe8958705376989c66405f
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-07 17:30:49 +0000

    create class with constructor

--- object_oriented_programming_oop/methods/class_methods.py
+++ object_oriented_programming_oop/methods/class_methods.py
@@ -7,3 +7,11 @@
         # - similar to how instance methods use 'self'
 # - 
 
+# a simple class
+class Student:
+
+    # constructor
+    def __init__(self, name, exam_title, percentage_score):
+        self.name = name
+        self.exam_title = exam_title
+        self.percentage_score = percentage_score
