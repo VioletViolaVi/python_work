e4a79dc4bd68e93fb895fea96787cb8edcd0fe82
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-07 19:44:27 +0000

    create constructor using __init__ magic method

--- object_oriented_programming_oop/methods/magic_methods.py
+++ object_oriented_programming_oop/methods/magic_methods.py
@@ -9,3 +9,12 @@
 # these methods are automatically called by python's built in operations e.g. 'print()' function, '==' operator, '>=' operator, '<' operator, etc.
     # when python operators are used on object, it becomes possible to customise the behaviour of said object 
 
+
+# standard class
+class Song:
+    
+    # this constructor is a 'magic method' aka 'dunder method'
+        # double underscores before & after 'init' makes it a magic/dunder method
+    def __init__(self, title, singer, sales):
+        self.title = title
+        self.singer = singer
