3062a805c8a71256b89a122af4dc01eb7382b122
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-07 19:40:55 +0000

    add notes about magic methods

--- object_oriented_programming_oop/methods/magic_methods.py
+++ object_oriented_programming_oop/methods/magic_methods.py
@@ -1,10 +1,11 @@
 # Magic Methods
 
-
 # Notes:
-# -
-# -
-# -
-# -
-# -
-# -
+
+# magic methods aka dunder methods
+# these are are methods that start & end with double underscores
+    # - e.g. '__init__' like the ones used in constructor previously
+# usually used inside classes
+# these methods are automatically called by python's built in operations e.g. 'print()' function, '==' operator, '>=' operator, '<' operator, etc.
+    # when python operators are used on object, it becomes possible to customise the behaviour of said object 
+
