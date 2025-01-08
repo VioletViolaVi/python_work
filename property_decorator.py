baaa9a600e67e324f4348356f8cc04837f0fc790
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-08 15:51:57 +0000

    create class & object

--- property_decorator.py
+++ property_decorator.py
@@ -3,7 +3,7 @@
 # Notes:
 
 # @property decorator enables the ability to define a method as a property
-# @property decorator makes respective part of code able to be accessed as if it's an attribute 
+# @property decorator makes respective part of code able to be accessed/treated as if it's an attribute 
 # additional logic can be added using the @property decorator
     #   - additional logic can be added when reading, writing & deleting attributes
 # @property methods provides the following when working w/ attributes:
@@ -11,3 +11,22 @@
     #   - setter method -> to write
     #   - deleter method -> to delete
 
+
+# standard class
+class Human:
+
+    # standard constructor/magic/dunder method
+    def __init__(self, name, age, weight, height):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+# standard object creation
+human = Human(name="Bob", age=29, weight=56.31, height=6.47)
+
+# standard printing of values from attribute variables
+print(human.name)
+print(human.age)
+print(human.weight)
+print(human.height)
