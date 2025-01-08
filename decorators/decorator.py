25fc5322c019ea3f1d824c94ebaef27c346e3c92
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-08 19:08:53 +0000

    add notes

--- decorators/decorator.py
+++ decorators/decorator.py
@@ -2,8 +2,8 @@
 
 # Notes:
 
-# - 
-# - 
-# - 
-# - 
-# - 
+# decorator is a function that extends the behaviour of another function 
+# decorators don't make changes to the original base function they are added to 
+# base functions are passed through decorator function as an argument/value
+
+
