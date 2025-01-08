8e354025f96fbc4e9c217f018387937d986a7487
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-08 21:27:14 +0000

    add notes on relative & absolute file paths

--- file_handling/file_detection.py
+++ file_handling/file_detection.py
@@ -2,9 +2,15 @@
 
 # Notes
 
-# - 
-# - 
-# - 
+# os module needs to be imported to give python a way to interact with the operating system
+    #   - os means 'operating system'
+# relative file path
+    #   - this path states how to get to the file from your current directory (the file location you are currently working from e.g. THIS '.py' file)
+# absolute file path 
+    #   - this states how to access the file from the root of the file system
 
 # need to import to be able to detect files
 import os
+
+# variable contains string that matches the real file path of the 'test_file.txt' file
+file_path = ""
