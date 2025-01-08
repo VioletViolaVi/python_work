c7bf402fe4bcab98b15c3554f3d87ee869a81404
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-08 19:21:58 +0000

    create & implement decorator

--- decorators/decorator.py
+++ decorators/decorator.py
@@ -7,7 +7,24 @@
 # base functions are passed through decorator function as an argument/value
 
 
+# creating a decorator
+# use this format for creating decorators
+# pass through base function in brackets '(...)' as the argument/value
+def add_sugar(func_param):
 
+    # inner function required to stop decorator from outputting contents of base function, w/out the base function making the call
+    def layer():
+
+        # function being passed through as the argument/value gets called inside inner function
+        func_param()
+    
+    # returns entire function
+    # 'layer' NOT 'layer()' as TypeError occurs
+        #   - TypeError: 'NoneType' object is not callable
+    return layer
+
+# decorator added on code line above base function
+@add_sugar
 # base function
 def bake_cake():
     print()
