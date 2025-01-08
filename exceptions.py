aa09de259ff01505783f633198a68fa089cda793
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-08 20:32:28 +0000

    add notes

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,13 @@
 # Exceptions
 
+# Notes
 
+# exceptions are events that interrupt regular flow of programs
+# some types of exceptions include (there's a lot more 📜):
+    #   - ArithmeticError
+    #   - IndentationError
+    #   - ValueError
+    #   - ZeroDivisionError
+    #   - AttributeError
+    #   - etc.
+# 
