049a72998ddb6619ecc9da666c1606d9f8300411
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-08 15:37:24 +0000

    add notes

--- property_decorator.py
+++ property_decorator.py
@@ -2,7 +2,12 @@
 
 # Notes:
 
-# - 
-# - 
-# - 
-# - 
+# @property decorator enables the ability to define a method as a property
+# @property decorator makes respective part of code able to be accessed as if it's an attribute 
+# additional logic can be added using the @property decorator
+    #   - additional logic can be added when reading, writing & deleting attributes
+# @property methods provides the following when working w/ attributes:
+    #   - getter method -> to read
+    #   - setter method -> to write
+    #   - deleter method -> to delete
+
