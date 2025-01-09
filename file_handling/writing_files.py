202d514b0e3b288bc0992f3ffce3fc7740c858ac
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-09 18:29:45 +0000

    create variables & add values

--- file_handling/writing_files.py
+++ file_handling/writing_files.py
@@ -1,3 +1,12 @@
 # Writing Files
 
+# Notes:
+# - 
 
+
+# to output data
+txt_data = "This is a string to be seen in a .txt document."
+
+# stores name of file, incl. extension
+    #   - file path chosen below is a relative file path
+file_path = "test.txt"
