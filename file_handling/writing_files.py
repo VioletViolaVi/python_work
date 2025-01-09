4468d509cee5cad4e672ba216dc817682de72db5
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-09 18:29:57 +0000

    create 'with' statement

--- file_handling/writing_files.py
+++ file_handling/writing_files.py
@@ -10,3 +10,13 @@ txt_data = "This is a string to be seen in a .txt document."
 # stores name of file, incl. extension
     #   - file path chosen below is a relative file path
 file_path = "test.txt"
+
+# to create a file
+# features of code below:
+    # 'with' is a statement used to wrap blocks of code for execution
+    # 'with' statement will close file once code is done using it
+        # - prevents need to manually close files
+        # - important to close files once done w/ them being opened to avoid unexpected behaviours occurring
+    # 'w' means write
+with open(file_path, "w") as file:
+    pass
