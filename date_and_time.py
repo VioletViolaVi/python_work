46a6e78b3c1aff95fb500bb9a541b6249b899212
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-11 18:34:07 +0000

    create object to get today's date

--- date_and_time.py
+++ date_and_time.py
@@ -8,4 +8,18 @@ import datetime
     # - (year, month, day) -> '(2025, 12, 25)'
 date = datetime.date(2025, 12, 25)
 
+print()
 print(f"date: {date}")
+
+
+print()
+print(" --- next --- ")
+print()
+
+
+# gets whatever date is now
+today_date = datetime.datetime.now()
+
+print(f"today_date: {today_date}")
+print()
+
