e63d055bdfc82ce9db2d9146c1bb9c652ac3093a
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-12 18:54:50 +0000

    import modules

--- date_and_time/alarm_clock.py
+++ date_and_time/alarm_clock.py
@@ -1,2 +1,7 @@
 # Alarm Clock
 
+# 'time' -> to change time every second for alarm clock
+# 'datetime' -> allows to work w/ string representations of time 
+# 'pygame' -> to provide sound effects
+import time, datetime, pygame
+
