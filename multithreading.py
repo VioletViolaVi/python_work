0eb92e36148069c3836d950058c2c3b7660e5803
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-21 14:01:00 +0000

    add notes

--- multithreading.py
+++ multithreading.py
@@ -1,4 +1,10 @@
 # Multithreading
 
+# Notes
+# - multithreading is used to perform multiple tasks at the same time (think of this as multitasking)
+# - multithreading is good for input output tasks
+    # - examples included reading files, fetching data from APIs
+        # - any activity that will take time to do & it is unknown when these activities will be completed
+# - the 'threading' module is to be imported to carry out multithreading
 
 
