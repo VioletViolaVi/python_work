0b5ebee99600fbdece8ddb6986a7d2e6cadcf53d
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-21 14:01:09 +0000

    import threading module

--- multithreading.py
+++ multithreading.py
@@ -7,4 +7,6 @@
         # - any activity that will take time to do & it is unknown when these activities will be completed
 # - the 'threading' module is to be imported to carry out multithreading
 
+# allows use of multithreading in python
+import threading
 
