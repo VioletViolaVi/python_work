fc3cbeb87fd49051e84015b5db33d85d280b1cdb
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2025-01-21 15:53:22 +0000

    import time module & use sleep method to delay function outputs

--- multithreading.py
+++ multithreading.py
@@ -7,22 +7,26 @@
         # - any activity that will take time to do & it is unknown when these activities will be completed
 # - the 'threading' module is to be imported to carry out multithreading
 
-# allows use of multithreading in python
-import threading
+# 'threading': allows use of multithreading in python
+# 'time': used to delay outputs from functions below
+import threading, time
 
 # functions to emulate watching movie @ home
 # eating popcorn
 def popcorn_eating():
+    time.sleep(7)
     print()
     print("You've finished eating the popcorn!")
 
 # watching movie
 def movie_watching():
+    time.sleep(10)
     print()
     print("Movie ended!")
 
 # microwaving the popcorn
 def microwaving_popcorn():
+    time.sleep(3)
     print()
     print("Microwave popcorn ready!")
 
